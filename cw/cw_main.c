d50bebf10660b9eb44dfc7cd4a710f10174e03c0
alanbjohnston <alan.b.johnston@gmail.com>
2018-10-21 10:15:19 -0400

    Update cw_main.c

--- cw/cw_main.c
+++ cw/cw_main.c
@@ -424,13 +424,13 @@ int add_dot(uint8_t *msg, int number) {
 	return counter;	
 }
 int get_tlm(int tlm[][5]) {
-        printf("11\n");	
-        printf("TLM 1a: %d \n", tlm[1][1]);
-        printf("12\n");	
+//        printf("11\n");	
+//        printf("TLM 1a: %d \n", tlm[1][1]);
+//        printf("12\n");	
 	tlm[1][1] = 99;
-	printf("13\n");
+//	printf("13\n");
 	printf("TLM 1a: %d \n", tlm[1][1]);
-	printf("14\n");
+//	printf("14\n");
 	
 	return 0;
 }
