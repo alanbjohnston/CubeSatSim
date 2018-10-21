00c28cd0e881723b7eb6048a6478918ce9634a3a
alanbjohnston <alan.b.johnston@gmail.com>
2018-10-21 10:11:14 -0400

    Update cw_main.c

--- cw/cw_main.c
+++ cw/cw_main.c
@@ -61,8 +61,8 @@ int main(void)
     uint8_t retVal;
     int tlm[7][5];
     int i, j;
-    for (i = 1, i < 7, i++) {
-        for (j = 1, j < 5, j++) {
+    for (i = 1, i < 7, i++;) {
+        for (j = 1, j < 5, j++;) {
 		tlm[i][j] = 0;
 	}
     }
