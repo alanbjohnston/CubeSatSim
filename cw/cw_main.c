87c4c1faa48ce7fb7e77b7afee0d3e68af79e769
alanbjohnston <alan.b.johnston@gmail.com>
2018-10-21 10:00:21 -0400

    Update cw_main.c

--- cw/cw_main.c
+++ cw/cw_main.c
@@ -54,7 +54,7 @@ int encode_header(uint8_t *buffer, int avail);
 int add_dash(uint8_t *msg, int number); 
 int add_dot(uint8_t *msg, int number); 
 int add_space(uint8_t *msg);
-int get_tlm(int *tlm[7][5]); 
+int get_tlm(int tlm[7][5]); 
 
 int main(void)
 {
@@ -111,7 +111,7 @@ int main(void)
 
         } else {
 		    
-	    get_tlm(&tlm);
+	    get_tlm(tlm);
     	    printf("TLM Received 1a: %d \n", tlm[1][1]);
 		    
 	    int tlm_3a = 0, tlm_1b = 0;
@@ -417,11 +417,11 @@ int add_dot(uint8_t *msg, int number) {
 	}
 	return counter;	
 }
-int get_tlm(int *tlm[7][5]) {
+int get_tlm(int tlm[][5]) {
         printf("11\n");	
-	*tlm[1][1] = 99;
+	tlm[1][1] = 99;
 	printf("12\n");
-	printf("TLM 1a: %d \n", *tlm[1][1]);
+	printf("TLM 1a: %d \n", tlm[1][1]);
 	printf("13\n");
 	
 	return 0;
