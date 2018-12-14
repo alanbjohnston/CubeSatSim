7ae65803a32a320755995f30f85addb201095b3a
Alan Johnston <alan.b.johnston@gmail.com>
2018-12-14 10:27:58 -0500

    updated demo script

--- demo.sh
+++ demo.sh
@@ -4,6 +4,6 @@ echo -e "\nDemo of CubeSatSim sends CW telemetry once then AFSK continuously\n\n
 
 # /home/pi/CubeSatSim/radio.cw.once
 
-/home/pi/CubeSatSim/radioafsk
+/home/pi/CubeSatSim/radioafsk >> /home/pi/CubeSatSim/log.txt
 
 
