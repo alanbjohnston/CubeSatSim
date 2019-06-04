854b0699cdbfbe16e7ac60671bdae6a2949214e3
alanbjohnston <alan.b.johnston@gmail.com>
2019-06-03 17:18:14 -0700

    added shell script for Pi Ground Station

---
+++ groundstation/autodecode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script to auto decode CubeSat Simulator telemetry
+
+echo -e "Running script to auto decode CubeSat Simulator telemetry\n"
+
+sudo rtl_fm -f 440.386M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
