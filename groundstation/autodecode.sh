0825f8ca04683d6defc55110f5e5c35a54990876
alanbjohnston <alan.b.johnston@gmail.com>
2019-06-03 17:20:04 -0700

    Update autodecode.sh

--- groundstation/autodecode.sh
+++ groundstation/autodecode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script to auto decode CubeSat Simulator telemetry
 
-echo -e "Running script to auto decode CubeSat Simulator telemetry\n"
+echo -e "Script to auto decode CubeSat Simulator telemetry\n"
 
 sudo rtl_fm -f 440.386M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
