1fa11a60ca407bae96ae16b2a8cbd49fe6a9615d
alanbjohnston <alan.b.johnston@gmail.com>
2019-06-04 18:17:06 -0400

    Update sdr.sh

--- groundstation/sdr.sh
+++ groundstation/sdr.sh
@@ -3,6 +3,9 @@
 
 echo -e "\nScript to run SDR for CubeSat Simulator\n"
 
+# kill openwebrx if running
+ps -ef | grep rtl | grep -v grep | awk '{print $2}' | sudo xargs kill
+
 echo -e "Connect to CubeSatSim WiFi and open browser to http://192.168.8.245:8073\n"
 
 cd ../openwebrx
