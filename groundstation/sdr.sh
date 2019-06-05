edede8837fbd64d0670e582bf98b7ecd4255d6d9
alanbjohnston <alan.b.johnston@gmail.com>
2019-06-04 20:41:46 -0400

    Update sdr.sh

--- groundstation/sdr.sh
+++ groundstation/sdr.sh
@@ -7,8 +7,11 @@ echo -e "IP Address to use in web browsers is: "
 
 hostname -I|cut -f1 -d ' '
 
-# kill openwebrx if running
+# kill rtl if running
 ps -ef | grep rtl | grep -v grep | awk '{print $2}' | sudo xargs kill
+
+# kill openwebrx process if running
+ps -ef | grep openwebrx | grep -v grep | awk '{print $2}' | sudo xargs kill
 
 cd ../openwebrx
 
