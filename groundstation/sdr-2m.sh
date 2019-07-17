ced44d2f974c1492761cc7a376ada135f7d926a9
Alan Johnston <alan.b.johnston@gmail.com>
2019-07-17 08:33:05 +0000

    groundstation scripts and configuration files

---
+++ groundstation/sdr-2m.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# script to run OpenWebRX SDR
+
+echo -e "\nScript to run SDR for ARISS Ground Station\n"
+
+echo -e "IP Address to use in web browsers is: "
+
+hostname -I|cut -f1 -d ' '
+
+
+./kill_all.sh
+
+cd ~/openwebrx
+
+sudo python openwebrx.py config_webrx_145
