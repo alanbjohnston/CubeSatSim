57d0a56cb771b75d643653b0fb9e47db5b62c2b9
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-20 03:50:18 -0400

    reverted changes

--- afsk/ax25.c
+++ afsk/ax25.c
@@ -18,7 +18,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
 #include "ax25.h"
 #include <string.h>
 #include "ax5043.h"
@@ -78,9 +77,6 @@ int ax25_init(ax25_conf_t *conf, const uint8_t *dest_addr, uint8_t dest_ssid,
     *out++ = (uint8_t) ((0x0F & src_ssid) << 1) | 0x61;
     //*out++ = ((0b1111 & src_ssid) << 1) | 0b01100001;
     conf->addr_field_len = AX25_MIN_ADDR_LEN;
-        
-    printf("%s>%s:", src_addr, dest_addr);   
-
     return PQWS_SUCCESS;
 }
 
@@ -92,17 +88,6 @@ int ax25_tx_frame(ax25_conf_t *hax25, ax5043_conf_t *hax,
 
     memcpy(__tx_buffer, hax25->addr_field, hax25->addr_field_len);
     memcpy(__tx_buffer + hax25->addr_field_len, payload, len);
-        
-    int jj;
-    for(jj = 0; jj < 118; jj++) {
-  /*      sprintf(hex_octet, "%02x",__tx_buffer[jj]);
-        strcat(hex_data, hex_octet);  */
-        printf("%c",__tx_buffer[jj]);
-    }
-    printf("\n");
-        
-        
-
 
     return ax5043_tx_frame(hax, __tx_buffer, len + hax25->addr_field_len,
             hax25->preamble_len, hax25->postable_len, 1000);
