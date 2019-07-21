edf2f92aafd9822f516668bedf8fba2beeb568ef
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-21 05:33:29 -0400

    Update main.c

--- afsk/main.c
+++ afsk/main.c
@@ -239,6 +239,7 @@ int main(int argc, char *argv[]) {
   tx_freq_hz -= tx_channel * 50000 + 85000; // subtracting rx offset of 90kHz
   #ifdef DEBUG_LOGGING
 	print ("INFO: Transmit frequency %3.5f MHz\n", tx_freq_hz/1000000);
+  #endif
 
   init_rf();
 
