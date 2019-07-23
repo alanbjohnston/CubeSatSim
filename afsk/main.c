c23c37ac21fe9c2ed816e3710c5039afa21081fb
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-23 08:37:58 -0400

    type in unit16_t

--- afsk/main.c
+++ afsk/main.c
@@ -110,10 +110,10 @@ struct SensorData read_sensor_data(struct SensorConfig sensor) {
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CONFIG, sensor.config);	
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
-    data.current  = (float)((int16_6)wiringPiI2CReadReg16(sensor.fd, INA219_REG_CURRENT)) / (float)sensor.currentDivider;
+    data.current  = (float)((int16_t)wiringPiI2CReadReg16(sensor.fd, INA219_REG_CURRENT)) / (float)sensor.currentDivider;
     uint16_t value = (uint16_t)wireReadRegister(sensor.fd, INA219_REG_BUSVOLTAGE);
     data.voltage  =  ((double)(value >> 3) * 4) / 1000;
-    data.power   = (float)wiringPiI2CReadReg16(sensor.fd, INA219_REG_POWER) * (float)sensor.powerMultiplier;	
+    data.power   = (float)((uint16_t)wiringPiI2CReadReg16(sensor.fd, INA219_REG_POWER)) * (float)sensor.powerMultiplier;	
 
     return data;
 }
