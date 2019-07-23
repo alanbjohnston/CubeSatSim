cbfa6829c4a61572b810e0a235877c81bd62e80b
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-23 10:57:36 -0400

    printf in 3 values

--- afsk/main.c
+++ afsk/main.c
@@ -110,8 +110,12 @@ struct SensorData read_sensor_data(struct SensorConfig sensor) {
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CONFIG, sensor.config);	
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
-    data.current  = (float)((int16_t)wiringPiI2CReadReg16(sensor.fd, INA219_REG_CURRENT)) / (float)sensor.currentDivider;
-/*	
+    int16_t valuec1  = (wiringPiI2CReadReg16(sensor.fd, INA219_REG_CURRENT);
+    float valuec3  = (float)(valuec2);
+    data.current  = valuec3 / (float)sensor.currentDivider;
+    printf("****** valuec1 %d   valuec2 %f  valuec3 %f \n", valuec1, valuec2, valuec3);
+			
+	/*	
     int16_t value0  = 1; // (int16_t)wiringPiI2CReadReg16(sensor.fd, INA219_REG_CURRENT);
     wiringPiI2CWrite(sensor.fd, INA219_REG_CURRENT);
     delay(1); // Max 12-bit conversion time is 586us per sample	
