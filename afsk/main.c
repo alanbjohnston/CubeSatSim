f0849cd11d14c1d7f4c39630fc51eb498d0defa6
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-24 08:24:30 -0400

    added calc and config

--- afsk/main.c
+++ afsk/main.c
@@ -139,7 +139,8 @@ struct SensorData read_sensor_data(struct SensorConfig sensor) {
     data.voltage  =  ((float)(value >> 3) * 4) / 1000;
 	
 //    data.power   = (float) wiringPiI2CReadReg16(sensor.fd, INA219_REG_POWER) * (float) sensor.powerMultiplier;
-	
+    wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
+    wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CONFIG, sensor.config);		
     wiringPiI2CWriteReg16(sensor.fd, INA219_REG_CALIBRATION, sensor.calValue);
     wiringPiI2CWrite(sensor.fd, INA219_REG_POWER);
     delay(1); // Max 12-bit conversion time is 586us per sample	
