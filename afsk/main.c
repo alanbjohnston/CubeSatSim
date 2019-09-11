a0142a2e4f0b456b268a37c188e4e950f9e5ad21
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-11 10:16:51 -0400

    typo

--- afsk/main.c
+++ afsk/main.c
@@ -203,7 +203,7 @@ int main(void) {
 	
 	char header_str[] = "\x03\xf0hi hi ";
 	char header_str2[] = "echo -e 'KU2Y>CQ:hi hi ";
-	char footer_str[] = "' | gen_packets -o telem.wav - && cat telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3"
+	char footer_str[] = "' | gen_packets -o telem.wav - && cat telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3";
 	    
 	if (RPITX)
 	  strcpy(str, header_str2);
