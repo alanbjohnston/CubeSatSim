33a5ab7f47f39acb5b7dd93f0b20db850a932822
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-11 22:17:38 -0400

    echo to t.txt

--- afsk/main.c
+++ afsk/main.c
@@ -203,8 +203,8 @@ int main(void) {
 	char tlm_str[1000];
 	
 	char header_str[] = "\x03\xf0hi hi ";
-	char header_str2[] = "echo -e \'KU2Y>CQ:hi hi ";
-	char footer_str[] = "\' | gen_packets -o telem.wav - "; // && cat telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3 &>/dev/null";
+	char header_str2[] = "rm t.txt && echo 'KU2Y>CQ:hi' >  t.txt && echo 'KU2Y>CQ:hi hi ";
+	char footer_str[] = "\' >> t.txt && echo \'KU2Y>CQ:hi hi ' && gen_packets -o telem.wav t.txt && cat telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3 &>/dev/null";
 	    
 	if (RPITX)
 	  strcpy(str, header_str2);
