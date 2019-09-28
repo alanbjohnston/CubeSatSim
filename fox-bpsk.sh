1cf36949e3f8aae80d69d7b6d78ee72f9078e2cd
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-28 08:36:48 -0400

    added b parameter to switch to BPSK mode

--- fox-bpsk.sh
+++ fox-bpsk.sh
@@ -8,4 +8,4 @@ echo -e "Script to run CubeSat Simulator in Fox BPSK Mode\n"
 
 sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float 2>&1& 
 
-/home/pi/CubeSatSim/radioafsk
+/home/pi/CubeSatSim/radioafsk b
