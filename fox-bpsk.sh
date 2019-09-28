8bc2c9b981b2703a41639329a7b269ba4fce77c8
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-28 15:57:51 -0400

    changed to 4348925

--- fox-bpsk.sh
+++ fox-bpsk.sh
@@ -7,6 +7,6 @@ ps -ef | grep sendiq | grep -v grep | awk '{print $2}' | sudo xargs kill
 sudo fuser -k 8080/tcp
 echo -e "Script to run CubeSat Simulator in Fox BPSK Mode\n"
 
-sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.894e6 -t float 2>&1& 
+sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.8925e6 -t float 2>&1& 
 
 /home/pi/CubeSatSim/radioafsk b $1
