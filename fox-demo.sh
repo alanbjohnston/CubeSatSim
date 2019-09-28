cea4826ee80616188bfcb53345fbd0df1b2107e5
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-28 08:55:01 -0400

    Update fox-demo.sh

--- fox-demo.sh
+++ fox-demo.sh
@@ -1,2 +1,7 @@
-(while true; do ps -ef | grep rpitx | grep -v grep | awk '{print $2}' | sudo xargs kill && sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float 2>&1& && /home/pi/CubeSatSim/radioafsk b; done)
+(while true; do 
+
+  ps -ef | grep rpitx | grep -v grep | awk '{print $2}' | sudo xargs kill && sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float 2>&1&
+  /home/pi/CubeSatSim/radioafsk b; 
+  
+done)
 
