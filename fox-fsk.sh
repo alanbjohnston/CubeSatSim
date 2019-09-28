cc06ed3031f0c4b720d74efc26b92693b7018396
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-28 15:03:42 -0400

    Update fox-fsk.sh

--- fox-fsk.sh
+++ fox-fsk.sh
@@ -9,4 +9,4 @@ echo -e "Script to run CubeSat Simulator in Fox FSK Mode\n"
 
 sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3& 
 
-/home/pi/CubeSatSim/radioafsk f $0
+/home/pi/CubeSatSim/radioafsk f $1
