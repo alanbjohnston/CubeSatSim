7f21ef0ea355da68ec00e4dda8dfb0d463b18e43
alanbjohnston <alan.b.johnston@gmail.com>
2019-09-28 14:57:47 -0400

    added sleep

--- fox-demo.sh
+++ fox-demo.sh
@@ -3,12 +3,14 @@
   ps -ef | grep rpitx | grep -v grep | awk '{print $2}' | sudo xargs kill
   ps -ef | grep sendiq | grep -v grep | awk '{print $2}' | sudo xargs kill
   sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.9e3& 
-  /home/pi/CubeSatSim/radioafsk f 2
+  /home/pi/CubeSatSim/radioafsk f 3
 
   ps -ef | grep rpitx | grep -v grep | awk '{print $2}' | sudo xargs kill
   ps -ef | grep sendiq | grep -v grep | awk '{print $2}' | sudo xargs kill
   sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/CubeSatSim/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float 2>&1&
-  /home/pi/CubeSatSim/radioafsk b 2
+  /home/pi/CubeSatSim/radioafsk b 3
+  
+  sleep 2;
   
 done)
 
