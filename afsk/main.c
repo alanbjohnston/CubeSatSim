199a623fdc34086af64df6ca87c29de2962217be
alanbjohnston <alan.b.johnston@gmail.com>
2019-10-05 11:59:48 -0400

    try 2x send

--- afsk/main.c
+++ afsk/main.c
@@ -923,7 +923,7 @@ int get_tlm_fox() {
 	digitalWrite (0, LOW);
 	printf("Sending %d buffer bytes over socket!\n", ctr);
 //	int sock_ret = send(sock, buffer, buffSize, 0);
-	int sock_ret = send(sock, buffer, (int)(ctr * ((float)( ((int)(frameCnt/2 + 1)))/(float)(frameCnt) )), 0);
+	int sock_ret = send(sock, buffer, ctr * 2 + 1, 0);
 	printf("Result of socket send: %d \n", sock_ret);
 	if (sock_ret == -1)  {
 		printf("Error: %s \n", strerror(errno));
