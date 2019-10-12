f0b1c6b6194a09ac83e5a6d7c5c1ea48c8731938
alanbjohnston <alan.b.johnston@gmail.com>
2019-10-12 13:28:57 -0400

    added sleep 1

--- demo.sh
+++ demo.sh
@@ -7,11 +7,18 @@ sudo killall sendiq
 sudo fuser -k 8080/tcp
 
 while true; do
+      sleep 1;
+      echo -e "\n Changing mode ********************************************************************************\n\n"
+      
       timeout 33 /home/pi/CubeSatSim/radioafsk fsk 3
       sudo killall rpitx
       sudo killall sendiq
       sudo fuser -k 8080/tcp
-      timeout 33 /home/pi/CubeSatSim/radioafsk bpsk 3
+
+      sleep 1;
+      echo -e "\n Changing mode ********************************************************************************\n\n"
+      
+      timeout 36 /home/pi/CubeSatSim/radioafsk bpsk 3
       sudo killall rpitx
       sudo killall sendiq
       sudo fuser -k 8080/tcp     
