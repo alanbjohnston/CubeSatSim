fca1a911f7e870b09321674126f819c083906233
alanbjohnston <alan.b.johnston@gmail.com>
2019-10-12 13:07:13 -0400

    typo

--- demo.sh
+++ demo.sh
@@ -7,11 +7,11 @@ sudo killall sendiq
 sudo fuser -k 8080/tcp
 
 while true; do
-      timeout 33 /home/pi/CubeSatSim/radioafsk.sh fsk 3
+      timeout 33 /home/pi/CubeSatSim/radioafsk fsk 3
       sudo killall rpitx
       sudo killall sendiq
       sudo fuser -k 8080/tcp
-      timeout 33 /home/pi/CubeSatSim/radioafsk.sh bpsk 3
+      timeout 33 /home/pi/CubeSatSim/radioafsk bpsk 3
       sudo killall rpitx
       sudo killall sendiq
       sudo fuser -k 8080/tcp     
