35f02cba70202826621080573275f840ca9c0425
alanbjohnston <alan.b.johnston@gmail.com>
2019-10-13 14:13:55 -0400

    using new cycle mode

--- demo.sh
+++ demo.sh
@@ -2,8 +2,6 @@
 
 echo -e "\nDemo of CubeSatSim sends FSK, BPSK, and AFSK telemetry alternately at 434.9 MHz continuously\n\n"
 
-exit
-
 sudo killall -9 rpitx
 sudo killall -9 sendiq
 sudo fuser -k 8080/tcp
@@ -12,24 +10,8 @@ while true; do
       sleep 1;
       echo -e "\n Changing mode ********************************************************************************\n\n"
       
-      timeout 28.5 /home/pi/CubeSatSim/radioafsk fsk 3
+      timeout 28.5 /home/pi/CubeSatSim/radioafsk c 3
       sudo killall -9 rpitx
       sudo killall -9 sendiq
       sudo fuser -k 8080/tcp
-
-      sleep 1;
-      echo -e "\n Changing mode ********************************************************************************\n\n"
-      
-      timeout 22 /home/pi/CubeSatSim/radioafsk bpsk 3
-      sudo killall -9 rpitx
-      sudo killall -9 sendiq
-      sudo fuser -k 8080/tcp     
-      sleep 1;
-      
-      echo -e "\n Changing mode ********************************************************************************\n\n"
-      
-      timeout 22 /home/pi/CubeSatSim/radioafsk afsk 3
-      sudo killall -9 rpitx
-      sudo killall -9 sendiq
-      sudo fuser -k 8080/tcp     
 done
