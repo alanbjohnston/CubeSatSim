b705ed4bbb2edf881631bafb02e020e0e95f03a5
alanbjohnston <alan.b.johnston@gmail.com>
2020-01-19 13:14:18 -0500

    changed to i2c-3 bus

--- afsk/main.c
+++ afsk/main.c
@@ -314,10 +314,10 @@ int main(int argc, char *argv[]) {
   sensor[PLUS_Y]  = config_sensor("/dev/i2c-1", 0x41, 400);
   sensor[BUS]  	  = config_sensor("/dev/i2c-1", 0x44, 400);
   sensor[BAT]     = config_sensor("/dev/i2c-1", 0x45, 400);
-  sensor[PLUS_Z]  = config_sensor("/dev/i2c-0", 0x40, 400);
-  sensor[MINUS_X] = config_sensor("/dev/i2c-0", 0x41, 400);
-  sensor[MINUS_Y] = config_sensor("/dev/i2c-0", 0x44, 400);
-  sensor[MINUS_Z] = config_sensor("/dev/i2c-0", 0x45, 400); 
+  sensor[PLUS_Z]  = config_sensor("/dev/i2c-3", 0x40, 400);
+  sensor[MINUS_X] = config_sensor("/dev/i2c-3", 0x41, 400);
+  sensor[MINUS_Y] = config_sensor("/dev/i2c-3", 0x44, 400);
+  sensor[MINUS_Z] = config_sensor("/dev/i2c-3", 0x45, 400); 
 
   int ret;
   //uint8_t data[1024];
