28652cae1754ac624eefdcaa349684f6a9a91718
Alan Johnston <alan.b.johnston@gmail.com>
2020-02-14 03:47:10 +0000

    updated demo with new arg values

--- demo.sh
+++ demo.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-echo -e "\nDemo of CubeSatSim sends AFSK telemetry at 434.9 MHz continuously\n\n"
+echo -e "\nDemo of CubeSatSim at 434.9 MHz continuously\n\n"
 
 #exit
 
-sudo killall -9 rpitx
-sudo killall -9 sendiq
-sudo fuser -k 8080/tcp
+y=$(last reboot | grep ^reboot | wc -l)
+echo $y
 
-/home/pi/CubeSatSim/radioafsk a 3
+if (( $y % 2 == 0 )) 
+then
+	echo -e "\n Continuous FSK Mode\n\n"
+	/home/pi/CubeSatSim/radioafsk f
+else
+
+	echo -e "\n Alternating FSK, BPSK, and AFSK telemetry Mode \n\n"
+	while true; do
+       		echo -e "\n Changing mode ******\n\n"
+    
+      		/home/pi/CubeSatSim/radioafsk a 5
+		/home/pi/CubeSatSim/radioafsk f 5
+		/home/pi/CubeSatSim/radioafsk b 5
+	done
+fi
