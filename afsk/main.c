ad7aad8f5970fd0a1acd293e57c90dd5db3d4a8e
alanbjohnston <alan.b.johnston@gmail.com>
2020-02-19 12:42:10 -0500

    id with saved callsign

--- afsk/main.c
+++ afsk/main.c
@@ -373,9 +373,17 @@ if (vB4)
 //            AX25_PREAMBLE_LEN,
 //            AX25_POSTAMBLE_LEN);
 	
-// Send ID in CW (Morse Code)	
-popen("echo 'de KU2Y ' > id.txt && gen_packets -M 20 id.txt -o morse.wav -r 48000 > /dev/null 2>&1 && cat morse.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.897e3 > /dev/null 2>&1", "r"); 
-sleep(6);
+// Send ID in CW (Morse Code)
+	
+  char cw_str[20];
+  char cw_header[] = "echo 'de ";
+  char cw_footer[] = "' > id.txt && gen_packets -M 20 id.txt -o morse.wav -r 48000 > /dev/null 2>&1 && cat morse.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.897e3 > /dev/null 2>&1";
+  strcpy(cw_str, cw_header);
+  strcat(str, call);
+  strcat(str, cw_footer);
+// popen("echo 'de KU2Y ' > id.txt && gen_packets -M 20 id.txt -o morse.wav -r 48000 > /dev/null 2>&1 && cat morse.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/CubeSatSim/rpitx/rpitx -i- -m RF -f 434.897e3 > /dev/null 2>&1", "r"); 
+  popen(str,"r");
+  sleep(6);
 	
 while (loop-- != 0)
   {
