d5fd1d80e30e4d2933eadfc5b153e597ddc806f9
alanbjohnston <alan.b.johnston@gmail.com>
2020-03-28 16:39:42 -0400

    swapped bus and battery

--- afsk/telem.c
+++ afsk/telem.c
@@ -215,8 +215,8 @@ int main(int argc, char *argv[]) {
 	  		printf("vB5 Present\n");
   sensor[PLUS_X]  = config_sensor("/dev/i2c-1", 0x40, 400); 
   sensor[PLUS_Y]  = config_sensor("/dev/i2c-1", 0x41, 400);
-  sensor[BUS]  	  = config_sensor("/dev/i2c-1", 0x44, 400);
-  sensor[BAT]     = config_sensor("/dev/i2c-1", 0x45, 400);
+  sensor[BUS]  	  = config_sensor("/dev/i2c-1", 0x45, 400);
+  sensor[BAT]     = config_sensor("/dev/i2c-1", 0x44, 400);
   sensor[PLUS_Z]  = config_sensor("/dev/i2c-3", 0x40, 400);
   sensor[MINUS_X] = config_sensor("/dev/i2c-3", 0x41, 400);
   sensor[MINUS_Y] = config_sensor("/dev/i2c-3", 0x44, 400);
