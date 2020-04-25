afdfa6a1df54482829cfc348086d50e23a9acd8e
alanbjohnston <alan.b.johnston@gmail.com>
2020-04-25 16:59:35 -0400

    Update rtl-tcp.sh

--- groundstation/rtl-tcp.sh
+++ groundstation/rtl-tcp.sh
@@ -3,11 +3,13 @@
 #
 # On SDR client, use 10.3.141.1:1234 to connect 
 
-echo -e "\nScript to run RTL-TCP Server for ARISS Ground Station\n"
+echo -e "\nScript to run RTL-TCP Server for ARISS Radio Pi\n"
 
-echo -e "IP Address to use in web browsers is: "
+echo -e "IP Address to use in your SDR application is: "
 
 hostname -I|cut -f1 -d ' '
+
+echo -e "The port is the default 1234. "
 
 ./kill_all.sh
 
