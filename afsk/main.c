9bc1a25690f36c2e1349758c35e0831716ae0a03
alanbjohnston <alan.b.johnston@gmail.com>
2020-04-27 11:28:24 -0400

    Update main.c

--- afsk/main.c
+++ afsk/main.c
@@ -509,7 +509,7 @@ if (cw_id == ON)	// Don't send CW if using AX5043 or in mode cycling or set by 3
   digitalWrite (txLed, txLedOn);
 //printf("Before 1st strcpy\n");
 printf("CW String: %s\n", cw_str);
-	*FILE f;
+	FILE* f;
 	f = popen(cw_str,"r");
 	printf("File %d \n", f);
   printf("close: %d \n", pclose(f));  // execute command and wait for termination before continuing
