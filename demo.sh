b05d5e55ea45e12196141ea3d57a0e10d167a9bc
alanbjohnston <alan.b.johnston@gmail.com>
2020-05-05 22:19:30 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,29 +2,6 @@
 
 echo -e "\nDemo of CubeSatSim at 434.9 MHz\n"
 
-if [ "$1" = "a" ]; then
-    echo "Mode is continuous AFSK"
-    sleep 1 
-    sudo killall -9 sendiq
-    sudo killall -9 sendiq
-    sleep 1    
-    /home/pi/CubeSatSim/radioafsk afsk
-elif [ "$1" = "f" ]; then
-    echo "Mode is continuous FSK"
-    sleep 1 
-    sudo killall -9 sendiq
-    sudo killall -9 sendiq
-    sleep 1
-    /home/pi/CubeSatSim/radioafsk fsk
-elif [ "$1" = "b" ]; then
-    echo "Mode is continuous BPSK"
-    sleep 1 
-    sudo killall -9 sendiq
-    sudo killall -9 sendiq
-    sleep 1   
-    /home/pi/CubeSatSim/radioafsk bpsk
-fi
-
 #exit
 
 y=$(last reboot | grep ^reboot | wc -l)
@@ -32,21 +9,44 @@ echo $y
 
 if (( $y % 2 == 0 )) 
 then
-	echo -e "\n Continuous FSK Mode\n\n"
-	/home/pi/CubeSatSim/radioafsk f
+	echo -e "\n Continuous Mode\n\n"
+#	/home/pi/CubeSatSim/radioafsk f
+
+	if [ "$1" = "a" ]; then
+    		echo "Mode is continuous AFSK"
+    		sleep 1 
+    		sudo killall -9 sendiq
+    		sudo killall -9 sendiq
+    		sleep 1    
+    		/home/pi/CubeSatSim/radioafsk afsk
+	elif [ "$1" = "f" ]; then
+    		echo "Mode is continuous FSK"
+    		sleep 1 
+    		sudo killall -9 sendiq
+    		sudo killall -9 sendiq
+    		sleep 1
+    		/home/pi/CubeSatSim/radioafsk fsk
+	elif [ "$1" = "b" ]; then
+   		 echo "Mode is continuous BPSK"
+    		sleep 1 
+    		sudo killall -9 sendiq
+    		sudo killall -9 sendiq
+    		sleep 1   
+    		/home/pi/CubeSatSim/radioafsk bpsk
+	fi
 else
 
 	echo -e "\n Alternating FSK, BPSK, and AFSK telemetry Mode \n\n"
-      	/home/pi/CubeSatSim/radioafsk afsk 5 y
+      	/home/pi/CubeSatSim/radioafsk afsk 4 y
 	while true; do
        		echo -e "\n Changing mode ******\n\n"
- 		/home/pi/CubeSatSim/radioafsk fsk 5 n
-		/home/pi/CubeSatSim/radioafsk bpsk 5 n
+ 		/home/pi/CubeSatSim/radioafsk fsk 4 n
+		/home/pi/CubeSatSim/radioafsk bpsk 4 n
 #		sleep 24 
 		sleep 1 
 		sudo killall -9 sendiq
 		sudo killall -9 sendiq
 		sleep 1
-		/home/pi/CubeSatSim/radioafsk afsk 5 n
+		/home/pi/CubeSatSim/radioafsk afsk 4 n
 	done
 fi
