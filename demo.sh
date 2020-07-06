1181486fdc27edf95aa203b72267660fc2f54c34
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-06 11:38:27 -0400

    removed alternating mode

--- demo.sh
+++ demo.sh
@@ -7,8 +7,8 @@ echo -e "\nDemo of CubeSatSim at 434.9 MHz\n"
 y=$(last reboot | grep ^reboot | wc -l)
 echo $y
 
-if (( $y % 2 == 0 )) 
-then
+#if (( $y % 2 == 0 )) 
+#then
 	echo -e "\n Continuous Mode\n\n"
 #	/home/pi/CubeSatSim/radioafsk f
 
@@ -34,25 +34,3 @@ then
     		sleep 1   
     		/home/pi/CubeSatSim/radioafsk bpsk
 	fi
-else
-
-	echo -e "\n Alternating FSK, BPSK, and AFSK telemetry Mode \n\n"
-      	/home/pi/CubeSatSim/radioafsk afsk 4 y
-	while true; do
-       		echo -e "\n Changing mode ******\n\n"
- 		/home/pi/CubeSatSim/radioafsk fsk 4 n
-		sudo killall -9 sendiq
-		sudo killall -9 csdr
-		sudo killall -9 rpitx
-		sleep 1
-##		/home/pi/CubeSatSim/radioafsk bpsk 4 n
-#		sleep 24 
-		sleep 1 
-		sudo killall -9 sendiq
-		sudo killall -9 csdr
-		sleep 1
-		sudo killall -9 sendiq
-		sleep 1
-		/home/pi/CubeSatSim/radioafsk afsk 4 n
-	done
-fi
