e3d2acca34193db1ba32966a758ccbb02590942f
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-15 23:10:44 -0400

    added restarting rpitx.service

--- demo.sh
+++ demo.sh
@@ -2,35 +2,19 @@
 
 echo -e "\nDemo of CubeSatSim at 434.9 MHz\n"
 
-#exit
+sudo systemctl stop rpitx
+sudo systemctl start rpitx
 
-y=$(last reboot | grep ^reboot | wc -l)
-echo $y
-
-#if (( $y % 2 == 0 )) 
-#then
 	echo -e "\n Continuous Mode\n\n"
 #	/home/pi/CubeSatSim/radioafsk f
 
 	if [ "$1" = "a" ]; then
     		echo "Mode is continuous AFSK"
-    		sleep 1 
-    		sudo killall -9 sendiq
-    		sudo killall -9 sendiq
-    		sleep 1    
     		/home/pi/CubeSatSim/radioafsk afsk
 	elif [ "$1" = "b" ]; then
-   		 echo "Mode is continuous BPSK"
-    		sleep 1 
-    		sudo killall -9 sendiq
-    		sudo killall -9 sendiq
-    		sleep 1   
+   		 echo "Mode is continuous BPSK"  
     		/home/pi/CubeSatSim/radioafsk bpsk
 	else 
     		echo "Mode is continuous FSK"
-    		sleep 1 
-    		sudo killall -9 sendiq
-    		sudo killall -9 sendiq
-    		sleep 1
     		/home/pi/CubeSatSim/radioafsk fsk
 	fi
