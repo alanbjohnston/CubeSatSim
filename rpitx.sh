28b84c31ce5322435316876e9a383837b13caeb1
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-15 23:12:12 -0400

    removed  2>&1 from sendiq

--- rpitx.sh
+++ rpitx.sh
@@ -10,5 +10,5 @@ echo -e "\nrpitx for CubeSatSim at 434.9 MHz\n"
 		sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | while true; do sudo timeout -k 1 60m /home/pi/rpitx/rpitx -i- -m RF -f 434.897e3; done
 	else 
     		echo "Tx is continuous FSK"
-		sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | while true; do sudo timeout -k 1 60m /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.8945e6 -t float 2>&1; done
+		sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | while true; do sudo timeout -k 1 60m /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.8945e6 -t float; done
 	fi
