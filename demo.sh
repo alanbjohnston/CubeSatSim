33c3ac8fd312bd28226db169c958d3478139e035
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-20 20:52:51 -0400

    cleaned up with restart

--- demo.sh
+++ demo.sh
@@ -2,19 +2,15 @@
 
 echo -e "\nDemo of CubeSatSim at 434.9 MHz\n"
 
-sudo systemctl stop rpitx
-sudo systemctl start rpitx
+sudo systemctl restart rpitx
 
-	echo -e "\n Continuous Mode\n\n"
-#	/home/pi/CubeSatSim/radioafsk f
-
-	if [ "$1" = "a" ]; then
-    		echo "Mode is continuous AFSK"
-    		/home/pi/CubeSatSim/radioafsk afsk
-	elif [ "$1" = "b" ]; then
-   		 echo "Mode is continuous BPSK"  
-    		/home/pi/CubeSatSim/radioafsk bpsk
-	else 
-    		echo "Mode is continuous FSK"
-    		/home/pi/CubeSatSim/radioafsk fsk
-	fi
+if [ "$1" = "a" ]; then
+   	echo "Mode is continuous AFSK"
+    	/home/pi/CubeSatSim/radioafsk afsk
+elif [ "$1" = "b" ]; then
+   	echo "Mode is continuous BPSK"  
+    	/home/pi/CubeSatSim/radioafsk bpsk
+else 
+    	echo "Mode is continuous FSK"
+    	/home/pi/CubeSatSim/radioafsk fsk
+fi
