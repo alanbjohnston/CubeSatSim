dbbd8933d825647473806a25f9938ddbaef737f7
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-28 07:35:11 -0400

    added sleep for sstv mode

--- demo.sh
+++ demo.sh
@@ -10,6 +10,9 @@ if [ "$1" = "a" ]; then
 elif [ "$1" = "b" ]; then
    	echo "Mode is continuous BPSK"  
     	/home/pi/CubeSatSim/radioafsk bpsk
+elif [ "$1" = "s" ]; then
+   	echo "Mode is continuous SSTV"  
+    	while true; do sleep 5; done
 else 
     	echo "Mode is continuous FSK"
     	/home/pi/CubeSatSim/radioafsk fsk
