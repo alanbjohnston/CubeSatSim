d31a0ef2b62a30d5f898de05cc9257dda11f1950
alanbjohnston <alan.b.johnston@gmail.com>
2020-07-27 23:41:42 -0400

    added rptix call for SSTV

--- rpitx.py
+++ rpitx.py
@@ -39,9 +39,8 @@ if __name__ == "__main__":
 	    while True:
     		time.sleep(5)
         elif (('s' == sys.argv[1]) or ('sstv' in sys.argv[1])):
-            print("SSTV") 
-	    while True:
-    		time.sleep(5)
+            print("SSTV")
+	    os.system("(while true; do (sleep 5 && cat ~/sstv/sstv3.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434.9e3); done)")
 	elif (('b' == sys.argv[1]) or ('bpsk' in sys.argv[1])):
             print("BPSK")
 	    os.system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float")
