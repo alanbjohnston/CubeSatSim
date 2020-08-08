be4f4aafeff64bfc87e3569e53ec9412dc2698a1
alanbjohnston <alan.b.johnston@gmail.com>
2020-08-08 13:02:24 -0400

    try getc

--- afsk/telem.c
+++ afsk/telem.c
@@ -126,14 +126,17 @@ struct SensorData read_sensor_data(struct SensorConfig sensor) {
 struct SensorConfig config_sensor(char *bus, int address,  int milliAmps) {
     struct SensorConfig data;
     FILE *i2cdetect = popen("timeout 5 i2cdetect -y 0", "r");
+    printf("i2cdetect 0 output: %s\n", getc(i2cdetect));
     int error = pclose(i2cdetect)/256;
     printf("i2cdetect 0 error: %d \n", error);
 
     i2cdetect = popen("timeout 5 i2cdetect -y 1", "r");
+    printf("i2cdetect 1 output: %s\n", getc(i2cdetect));
     error = pclose(i2cdetect)/256;
     printf("i2cdetect 1 error: %d \n", error);
 
     i2cdetect = popen("timeout 5 i2cdetect -y 3", "r");
+    printf("i2cdetect 3 output: %s\n", getc(i2cdetect));
     error = pclose(i2cdetect)/256;
     printf("i2cdetect 3 error: %d \n", error);
 	
