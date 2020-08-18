45105e2304c563d18c73711270ec0d1f39de6b82
alanbjohnston <alan.b.johnston@gmail.com>
2020-08-18 16:43:19 -0400

    Update telem.c

--- afsk/telem.c
+++ afsk/telem.c
@@ -335,7 +335,7 @@ int main(int argc, char *argv[]) {
   int count;
 
 	file = popen("python3 /home/pi/CubeSatSim/python/voltcurrent.py 1 3 c", "r");
-//   	char cmdbuffer[1000];
+   	char cmdbuffer[1000];
     	fgets(cmdbuffer, 1000, file);
 	printf("result: %s\n", cmdbuffer);
     	pclose(file);
