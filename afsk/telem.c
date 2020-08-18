92847b48d064813574a22cb819d6aa431baf7791
alanbjohnston <alan.b.johnston@gmail.com>
2020-08-17 20:32:36 -0400

    Update telem.c

--- afsk/telem.c
+++ afsk/telem.c
@@ -158,7 +158,7 @@ struct SensorConfig config_sensor(char *bus, int address,  int milliAmps) {
     //char st[] ="Where there is will, there is a way.";
     char *ch;
     const char dash[2] = "-";
-    ch = strtok(&bus, dash);
+    ch = strtok(bus, dash);
     printf("ch: %s\n", ch);
     ch = strtok(NULL, dash);	
     printf("ch: %s\n", ch);
