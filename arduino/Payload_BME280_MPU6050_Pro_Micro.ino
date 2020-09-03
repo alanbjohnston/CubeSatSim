f14c98bc169310c8ab756d5ba3bbf130c2cefcc0
alanbjohnston <alan.b.johnston@gmail.com>
2020-09-03 09:35:32 -0400

    took out while loop on no bme280 detected

--- arduino/Payload_BME280_MPU6050_Pro_Micro.ino
+++ arduino/Payload_BME280_MPU6050_Pro_Micro.ino
@@ -31,7 +31,7 @@ void setup() {
 
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+//    while (1);
   }
   
   mpu6050.begin();
