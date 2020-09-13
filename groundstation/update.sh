a5efab67fbde222a237e838e9d4dc46cce5d12d5
Alan Johnston <alan.b.johnston@gmail.com>
2020-09-13 11:15:46 -0400

    script to update openwebrx config

---
+++ groundstation/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+echo -e "\nUpdating configurations. \n"
+
+sudo cp ~/CubeSatSim/groundstation/config_webrx.py /etc/openwebrx/config_webrx.py
+
+sudo cp ~/CubeSatSim/groundstation/bookmarks.json /etc/openwebrx/bookmarks.json
+
+sudo cp ~/CubeSatSim/groundstation/readme.txt ~/Desktop
+
+
