bf412cf20cc33abda582d95f17eb3b7a4a5fb247
alanbjohnston <alan.b.johnston@gmail.com>
2020-09-19 16:54:32 -0400

    changed file names

--- groundstation/sstv.sh
+++ groundstation/sstv.sh
@@ -3,21 +3,21 @@
 
 echo
 
-echo "Script to transmit SSTV images from ARISS Radio Pi using CubeSatSim Lite"
+echo "Script to transmit SSTV images from Pi Camera using CubeSatSim Lite"
 
 echo 
 
-echo "Taking picture with Raspicam to Desktop raspicam.jpg"
+echo "Taking picture with Pi Camera to Desktop camera.jpg"
 
 echo 
 
-raspistill -o ~/Desktop/raspicam.jpg -w 640 -h 496 > /dev/null 2>&1
+raspistill -o ~/Desktop/camera.jpg -w 640 -h 496 > /dev/null 2>&1
 
 echo "Converting image to SSTV with pysstv (takes about a minute)"
 
 echo 
 
-python3 -m pysstv --mode PD120 ~/Desktop/raspicam.jpg output.wav > /dev/null 2>&1
+python3 -m pysstv --mode PD120 ~/Desktop/camera.jpg sstv_camera.wav > /dev/null 2>&1
 
 echo "Transmitting PD120 SSTV image on 434.9 MHz using rpitx (takes about two minutes to send)"
 
@@ -27,5 +27,5 @@ sudo systemctl stop rpitx
 
 sudo killall -9 rpitx > /dev/null 2>&1
 
-cat output.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434.9e3 > /dev/null 2>&1
+cat sstv_camera.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434.9e3 > /dev/null 2>&1
 
