8994bde91ecfe0248371f49e879525cd0854cf5e
alanbjohnston <alan.b.johnston@gmail.com>
2020-09-19 08:57:18 -0400

    added git pull

--- groundstation/update.sh
+++ groundstation/update.sh
@@ -3,10 +3,13 @@
 
 echo -e "\nUpdating configurations. \n"
 
+cd ~/CubeSatSim && git pull
+
 sudo cp ~/CubeSatSim/groundstation/config_webrx.py /etc/openwebrx/config_webrx.py
 
 sudo cp ~/CubeSatSim/groundstation/bookmarks.json /etc/openwebrx/bookmarks.json
 
 sudo cp ~/CubeSatSim/groundstation/readme.txt ~/readme.txt
 
+sudo cp ~/CubeSatSim/groundstation/*.desktop ~/Desktop
 
