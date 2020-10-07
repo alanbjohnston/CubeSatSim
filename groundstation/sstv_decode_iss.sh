31457bd805319b83767c7ab1262b102fbfa8d83c
Alan Johnston <alan.b.johnston@gmail.com>
2020-10-07 16:00:52 -0400

    adding SSTV scripts and desktops

---
+++ groundstation/sstv_decode_iss.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# script to decode SSTV using rtl_fm and QSSTV from the CubeSatSim
+
+echo "Script to decode SSTV from the CubeSatSim for ARISS Radio Pi"
+
+echo
+
+sudo modprobe snd-aloop
+
+sudo systemctl stop openwebrx
+
+sudo systemctl stop rtl_tcp
+
+pkill -o chromium &>/dev/null
+
+sudo killall -9 rtl_fm &>/dev/null
+
+sudo killall -9 qsstv &>/dev/null
+
+sudo killall -9 rtl_tcp &>/dev/null
+
+sudo killall -9 java &>/dev/null
+
+sudo killall -9 CubicSDR &>/dev/null
+
+rtl_fm -M fm -f 145.8M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1 
+qsstv &
+
+$SHELL
