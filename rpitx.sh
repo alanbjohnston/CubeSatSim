ddcd6c7d3c1e9504b6f30eb3becfc66660c42609
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-08 18:03:06 -0500

    added cycling mode conditionals

--- rpitx.sh
+++ rpitx.sh
@@ -6,6 +6,19 @@ if [ "$1" = "c" ]; then
     echo "Mode cycling"
     y=$(last reboot | grep ^reboot | wc -l)
     echo $y
+          if [ $(($y % 4)) = 0 ]; then
+   	   echo "Mode is continuous AFSK"
+    	   python -u /home/pi/CubeSatSim/rpitx.py a
+      elif [ $(($y % 4)) = 1 ]; then
+   	   echo "Mode is continuous BPSK"  
+    	   python -u /home/pi/CubeSatSim/rpitx.py b
+      elif [ $(($y % 4)) = 2 ]; then
+   	   echo "Mode is continuous SSTV"  
+    	   python -u /home/pi/CubeSatSim/rpitx.py s
+      else
+         echo "Mode is continuous FSK"
+         python -u /home/pi/CubeSatSim/rpitx.py f
+      fi
 else
     python -u /home/pi/CubeSatSim/rpitx.py $1
 fi
