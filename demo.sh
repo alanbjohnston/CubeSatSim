24d9d0da586259339cd545d09789a6ece330d64c
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-08 10:55:30 -0500

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -20,4 +20,17 @@ else
     echo "Mode cycling"
     y=$(last reboot | grep ^reboot | wc -l)
     echo $y
-fi
+    
+      if [ "$y" % 4 = "0" ]; then
+   	   echo "Mode is continuous AFSK"
+    	   /home/pi/CubeSatSim/radioafsk afsk
+      elif [ "$1" = "b" ]; then
+   	   echo "Mode is continuous BPSK"  
+    	   /home/pi/CubeSatSim/radioafsk bpsk
+      elif [ "$1" = "s" ]; then
+   	   echo "Mode is continuous SSTV"  
+    	   while true; do sleep 5; done
+      els3 [ "$1" = "f" ]; then
+         echo "Mode is continuous FSK"
+         /home/pi/CubeSatSim/radioafsk fsk  
+      fi
