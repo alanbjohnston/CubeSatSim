b9220ad3b1e8ab000f2b9ba8ae1d6fc459f1c5eb
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-08 10:49:33 -0500

    added mode cycling if parameter is c

--- rpitx.sh
+++ rpitx.sh
@@ -2,4 +2,11 @@
 
 echo -e "\nrpitx for CubeSatSim at 434.9 MHz using python\n"
 
-python -u /home/pi/CubeSatSim/rpitx.py $1
+if [ "$1" = "c" ]; then
+    echo "Mode cycling"
+    y=$(last reboot | grep ^reboot | wc -l)
+    echo $y
+else
+    python -u /home/pi/CubeSatSim/rpitx.py $1
+fi
+
