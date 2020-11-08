4458c6df522bb410e350546d1ce37c8f1cba75ec
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-08 11:22:22 -0500

    change variable $1

--- demo.sh
+++ demo.sh
@@ -4,6 +4,23 @@ echo -e "\nDemo of CubeSatSim at 434.9 MHz\n"
 
 sudo systemctl restart rpitx
 
+if [ "$1" = "c" ]; then
+    echo "Mode cycling"
+    y=$(last reboot | grep ^reboot | wc -l)
+    echo $y
+    echo $(($y % 4))
+    
+      if [ $(($y % 4)) = 0 ]; then
+   	   $1 = "a"
+      elif [ $(($y % 4)) = 1 ]; then
+   	   $1 = "b"
+      elif [ $(($y % 4)) = 2 ]; then
+   	   $1 = "s"
+      else
+         $1 = "f"
+      fi
+fi
+
 if [ "$1" = "a" ]; then
    	echo "Mode is continuous AFSK"
     	/home/pi/CubeSatSim/radioafsk afsk
@@ -13,26 +30,8 @@ elif [ "$1" = "b" ]; then
 elif [ "$1" = "s" ]; then
    	echo "Mode is continuous SSTV"  
     	while true; do sleep 5; done
-elif [ "$1" = "f" ]; then
+else [ "$1" = "f" ]; then
     echo "Mode is continuous FSK"
     /home/pi/CubeSatSim/radioafsk fsk
-else
-    echo "Mode cycling"
-    y=$(last reboot | grep ^reboot | wc -l)
-    echo $y
-    echo $(($y % 4))
-    
-      if [ $(($y % 4)) = 0 ]; then
-   	   echo "Mode is continuous AFSK"
-    	   /home/pi/CubeSatSim/radioafsk afsk
-      elif [ $(($y % 4)) = 1 ]; then
-   	   echo "Mode is continuous BPSK"  
-    	   /home/pi/CubeSatSim/radioafsk bpsk
-      elif [ $(($y % 4)) = 2 ]; then
-   	   echo "Mode is continuous SSTV"  
-    	   while true; do sleep 5; done
-      else
-         echo "Mode is continuous FSK"
-         /home/pi/CubeSatSim/radioafsk fsk  
-      fi
 fi
+
