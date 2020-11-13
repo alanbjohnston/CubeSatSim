585992642e2ddfd3788371dbe137952e134fba14
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-13 14:14:25 -0500

    switched the order of AFSK and FSK to match button

--- rpitx.sh
+++ rpitx.sh
@@ -6,7 +6,7 @@ if [ "$1" = "c" ]; then
     echo "Mode cycling"
     y=$(last reboot | grep ^reboot | wc -l)
     echo $y
-          if [ $(($y % 4)) = 0 ]; then
+          if [ $(($y % 4)) = 3 ]; then
    	   echo "Mode is continuous AFSK"
     	   python -u /home/pi/CubeSatSim/rpitx.py a
       elif [ $(($y % 4)) = 1 ]; then
