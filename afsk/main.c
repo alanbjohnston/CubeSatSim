9464d69471e45867c7e8e9c3ef31cbc238362b1c
alanbjohnston <alan.b.johnston@gmail.com>
2020-11-27 11:51:59 -0500

    testing again

--- afsk/main.c
+++ afsk/main.c
@@ -1180,8 +1180,8 @@ if (payload == ON)
   encodeA(b, 48 + head_offset, sensor2);
   encodeB(b, 49 + head_offset, sensor3);
 	  
-  encodeA(b, 51 + head_offset, 4095); // STEMBoardFailure + NormalModeFailure);  // NormalModeFailure set to 32 if in Safe Mode
-  encodeB(b, 52 + head_offset, 0); // 16+32+64); // Try TXAntenna RXAntenna 
+  encodeA(b, 51 + head_offset, 1 + 2048); // STEMBoardFailure + NormalModeFailure);  // NormalModeFailure set to 32 if in Safe Mode
+  encodeB(b, 52 + head_offset, 1 + 2048); // 16+32+64); // Try TXAntenna RXAntenna 
    
   	short int data10[headerLen + rsFrames * (rsFrameLen + parityLen)];
   	short int data8[headerLen + rsFrames * (rsFrameLen + parityLen)]; 
