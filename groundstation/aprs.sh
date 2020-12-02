c3fbdfa77b6a46ae69c5268d29448c99844ab3d0
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-02 18:28:24 -0500

    added killall and rtl_fm to loopback (not multimon-ng)

--- groundstation/aprs.sh
+++ groundstation/aprs.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 # script to auto decode APRS packets on 2m
 
-# kill openwebrx if it is running
-ps -ef | grep rtl | grep -v grep | awk '{print $2}' | sudo xargs kill
+sudo modprobe snd-aloop
+
+sudo systemctl stop openwebrx
+
+sudo systemctl stop rtl_tcp
+
+pkill -o chromium &>/dev/null
+
+sudo killall -9 rtl_fm &>/dev/null
+
+sudo killall -9 qsstv &>/dev/null
+
+sudo killall -9 rtl_tcp &>/dev/null
+
+sudo killall -9 java &>/dev/null
+
+sudo killall -9 CubicSDR &>/dev/null
 
 echo -e "Script to auto decode APRS packets on 144.390 MHz\n"
 
-sudo rtl_fm -f 144.39M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
+#sudo rtl_fm -f 144.39M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
+
+rtl_fm -M fm -f 144.39M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1
+
+direwolf
+
+$SHELL
+
