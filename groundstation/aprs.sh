8eeb79273a3dc9a12024063f953793c81a75eac6
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-02 19:20:00 -0500

    moved direwolf&

--- groundstation/aprs.sh
+++ groundstation/aprs.sh
@@ -13,6 +13,8 @@ sudo killall -9 rtl_fm &>/dev/null
 
 sudo killall -9 aplay &>/dev/null
 
+direwolf&
+
 sudo killall -9 qsstv &>/dev/null
 
 sudo killall -9 rtl_tcp &>/dev/null
@@ -26,8 +28,6 @@ echo -e "Script to auto decode APRS packets on 144.390 MHz\n"
 #sudo rtl_fm -f 144.39M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
 
 rtl_fm -M fm -f 144.39M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1
-
-direwolf
 
 $SHELL
 
