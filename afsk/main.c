db32addc30b2ae5a63aa38a351a5171a61c31a1a
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-07 15:46:06 -0500

    angle 0.2 0.2 1

--- afsk/main.c
+++ afsk/main.c
@@ -979,9 +979,9 @@ if (firstTime != ON)
 	  
 	  
   double time = (millis() - time_start)/1000.0;	  
-  double Xi = 5.0 * sin(1.57) * sin(2.0 * 3.14 * time / 46.0);
-  double Yi = 5.0 * sin(1.57) * sin((2.0 * 3.14 * time / 46.0) + (3.14/2.0));
-  double Zi = 5.0 * cos(1.57 - 0.0) * sin((2.0 * 3.14 * time / 46.0) + 3.14);
+  double Xi = 5.0 * sin(1.37) * sin(2.0 * 3.14 * time / 46.0);
+  double Yi = 4.8 * sin(1.37) * sin((2.0 * 3.14 * time / 46.0) + (3.14/2.0));
+  double Zi = 5.0 * cos(1.37 - 0.2) * sin((2.0 * 3.14 * time / 46.0) + 3.14 + 0.785);
 	  
   //printf("Yi: %f Zi: %f \n", Yi, Zi);
 	  
