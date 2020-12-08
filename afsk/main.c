aa1e6c96c55f0fe6ef3bc6d82645b9011cab1a4b
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-08 15:02:39 -0500

    Update main.c

--- afsk/main.c
+++ afsk/main.c
@@ -417,9 +417,9 @@ amps_max[0] = rnd_float(140.0, 190.0) * (float) sin(angle[1]);
 amps_max[1] = rnd_float(140.0, 190.0) * (float) cos(angle[0]);
 amps_max[2] = rnd_float(140.0, 190.0) * (float) cos(angle[1] - angle[0]);
 */
-amps_max[0] = 180 * (float) sin(angle[1]);	
-amps_max[1] = 180 * (float) cos(angle[0]);
-amps_max[2] = 180 * (float) cos(angle[1] - angle[0]);
+amps_max[0] = 180.0 * (float) sin(angle[1]);	
+amps_max[1] = 180.0 * (float) cos(angle[0]);
+amps_max[2] = 180.0 * (float) cos(angle[1] - angle[0]);
 	
 batt = rnd_float(3.5, 4.3);
 speed = rnd_float(1.0, 2.5);
