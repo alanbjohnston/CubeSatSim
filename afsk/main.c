8ca33c8767f35c6c5e949b358e36c80f8dfaeb07
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-09 16:10:23 -0500

    Rssi typo

--- afsk/main.c
+++ afsk/main.c
@@ -1315,7 +1315,7 @@ if (payload == ON)
   encodeB(b, 34 + head_offset,altitude);   // Altitude
 	  
   encodeA(b, 36 + head_offset,  Resets);	  
-  encodeA(b, 37 + head_offset,  Rssi);	
+  encodeB(b, 37 + head_offset,  Rssi);	
 	  
   encodeA(b, 39 + head_offset,  IHUcpuTemp);
 	  
