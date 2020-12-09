9b23cf1766ac62210bb731cf5437f396c1c2a459
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-09 14:36:22 -0500

    Update main.c

--- afsk/main.c
+++ afsk/main.c
@@ -1326,7 +1326,7 @@ if (payload == ON)
   encodeB(b, 49 + head_offset, sensor3);
 	  
 //  encodeA(b, 51 + head_offset, STEMBoardFailure + NormalModeFailure * 2 + (i2c_bus0 == OFF) * 16 + (i2c_bus1 == OFF) * 32 + (i2c_bus3 == OFF) * 64  + (camera == OFF) * 128 + groundCommandCount * 256); 
-  encodeA(b, 51 + head_offset, STEMBoardFailure + NormalModeFailure * 2 + (i2c_bus0 == OFF) * 16 + (i2c_bus1 == OFF) * 32 + (i2c_bus3 == OFF) * 64  + (0) * 128 + 1 * 256 + 1 * 512 + 1 * 1024 + 1*2048); 
+//  encodeA(b, 51 + head_offset, STEMBoardFailure + NormalModeFailure * 2 + (i2c_bus0 == OFF) * 16 + (i2c_bus1 == OFF) * 32 + (i2c_bus3 == OFF) * 64  + (0) * 128 + 1 * 256 + 1 * 512 + 1 * 1024 + 1*2048); 
   encodeB(b, 52 + head_offset, rxAntennaDeployed + txAntennaDeployed* 2);  
    
   	short int data10[headerLen + rsFrames * (rsFrameLen + parityLen)];
