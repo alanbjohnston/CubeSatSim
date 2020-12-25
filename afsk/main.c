2371bed14ec645cc7602c60ecff7874794dc73b1
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-25 16:42:43 -0500

    reduced sleepTime and samplePeriod

--- afsk/main.c
+++ afsk/main.c
@@ -549,8 +549,8 @@ int main(int argc, char * argv[]) {
       //   samplePeriod = ((float)((syncBits + 10 * (headerLen + rsFrames * (rsFrameLen + parityLen))))/(float)bitRate) * 1000 - 1800;
       //    samplePeriod = 3000;
       //    sleepTime = 3.0;
-      samplePeriod = 2200; // reduce dut to python and sensor querying delays
-      sleepTime = 1.0f; // 2.2f;
+      samplePeriod = 1200; // 2200; // reduce dut to python and sensor querying delays
+      sleepTime = 0.25f; // 2.2f;
 
       printf("\n BPSK Mode, bufLen: %d,  %d bits per frame, %d bits per second, %d seconds per frame %d ms sample period\n",
         bufLen, bufLen / (samples * frameCnt), bitRate, bufLen / (samples * frameCnt * bitRate), samplePeriod);
