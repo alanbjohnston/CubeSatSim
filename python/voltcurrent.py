3d929c81a55d4821e65fc55388a0dbb677f6170e
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-26 12:34:00 -0500

    added sleep(0.1) between imports

--- python/voltcurrent.py
+++ python/voltcurrent.py
@@ -1,12 +1,16 @@
 """Sample code and test for adafruit_in219"""
 # Reads all voltage and current sensors for two I2C buses
 
+from time import sleep
 import sys
+sleep(0.1)
 #import board
 import busio
+sleep(0.1)
 from adafruit_extended_bus import ExtendedI2C as I2C
+sleep(0.1)
 from adafruit_ina219 import INA219
-
+sleep(0.1)
 if __name__ == "__main__":
 #	    print 'Length: ', len(sys.argv)
   
