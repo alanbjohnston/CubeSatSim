fffdff6b1057fdd13d7b9b5fc12acdb8b400c2ea
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-27 11:16:10 -0500

    reduced sleep from 3.5 to 3.2 to avoid buffer overflow

--- afsk/main.c
+++ afsk/main.c
@@ -1057,7 +1057,7 @@ void get_tlm_fox() {
       while ((millis() - sampleTime) < (unsigned int)samplePeriod)
         sleep((unsigned int)sleepTime);
 */
-      sleep(3.5);
+      sleep(3.2);
 	    
       digitalWrite(txLed, txLedOff);
       #ifdef DEBUG_LOGGING
