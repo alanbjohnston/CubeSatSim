27399f202283dbe0ead6dc956387d5491726df20
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-28 18:21:01 -0500

    Update main.c

--- afsk/main.c
+++ afsk/main.c
@@ -1008,7 +1008,7 @@ int start_subprocess(char *const command[], int *pid, int *infd, int *outfd)
     int p1[2], p2[2];
 
     if (!pid || !infd || !outfd)
-        return false;
+        return 0;
 
     if (pipe(p1) == -1)
         goto err_pipe1;
@@ -1023,7 +1023,7 @@ int start_subprocess(char *const command[], int *pid, int *infd, int *outfd)
         *outfd = p2[0];
         close(p1[0]);
         close(p2[1]);
-        return true;
+        return 1;
     } else {
         /* Child process. */
         dup2(p1[0], 0);
