630c6f2a0f437ff7eb6f52cb161b17ddbf6ffbaa
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-29 09:06:41 -0500

    removed python read

--- afsk/main.c
+++ afsk/main.c
@@ -1184,13 +1184,13 @@ else {
     int count1;
     char * token;
 //    char cmdbuffer[1000];
-/**/
+/*
 //    FILE * file = popen(pythonStr, "r");
     fputc('\n', file1);
     fgets(cmdbuffer, 1000, file1);
     printf("Python read Result: %s\n", cmdbuffer);
 //    pclose(file);
-/**/
+*/
     const char space[2] = " ";
     token = strtok(cmdbuffer, space);
 
