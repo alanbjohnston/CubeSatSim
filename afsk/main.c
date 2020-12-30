e2d9badab293f9899403fd59cb8c187f7f15f536
alanbjohnston <alan.b.johnston@gmail.com>
2020-12-30 10:20:09 -0500

    typo

--- afsk/main.c
+++ afsk/main.c
@@ -119,6 +119,7 @@ int rpitxStatus = -1;
 float amplitude; // = ; // 20000; // 32767/(10%amp+5%amp+100%amp)
 float freq_Hz = 3000; // 1200
 short int sin_samples;
+short int sin_map[16];
 
 int smaller;
 int flip_ctr = 0;
