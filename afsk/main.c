83ff6f13119721aec1831f9bbcdb0673186d5c41
alanbjohnston <alan.b.johnston@gmail.com>
2021-03-20 10:00:44 -0400

    print typo

--- afsk/main.c
+++ afsk/main.c
@@ -1982,11 +1982,11 @@ float toAprsFormat(float input) {
     int mm1 = (int)((input - dd) * 60);
     int mm2; // = (int)((input - dd - mm1/60) * 60 * 60);
     float mm21 = input - dd - mm1/60
-    print("%f ", mm21);
+    printf("%f ", mm21);
     mm21 = mm21 * 60 * 60;
-    print("%f ", mm21);
+    printf("%f ", mm21);
     mm2 = (int)mm21;
-    print("%d ", mm2);
+    printf("%d ", mm2);
     float output = dd * 100 + mm1 + mm2 * 0.01;
     return(output);	
 }
