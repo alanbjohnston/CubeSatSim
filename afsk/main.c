a79dbdd7e767cdd6722fccaf0ed7a2fc1f3b0bdd
alanbjohnston <alan.b.johnston@gmail.com>
2021-03-20 09:52:33 -0400

    fixed .MM calculation

--- afsk/main.c
+++ afsk/main.c
@@ -1980,7 +1980,7 @@ float toAprsFormat(float input) {
 // converts decimal coordinate (lattitude or longitude) to APRS DDMM.MM format	
     int dd = (int) input;
     int mm1 = (int)((input - dd) * 60);
-    int mm2 = (int)((input - dd - mm1/60) * 60) ;
+    int mm2 = (int)((input - dd - mm1/60) * 60 * 60) ;
     float output = dd * 100 + mm1 + mm2 * 0.01;
     return(output);	
 }
