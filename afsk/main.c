de0ea0e42b5026ffb97fd73a6b54496f41f6703b
alanbjohnston <alan.b.johnston@gmail.com>
2021-03-30 12:25:00 -0400

    TX with VHF BPF

--- afsk/main.c
+++ afsk/main.c
@@ -303,19 +303,20 @@ mode = AFSK;
           onLedOff = LOW;
           transmit = TRUE;
         } else {
-       	pinMode(23, INPUT);
-        pullUpDnControl(23, PUD_UP);
+       	  pinMode(23, INPUT);
+          pullUpDnControl(23, PUD_UP);
 
-        if (digitalRead(23) != HIGH) {
-          printf("vB5 Present with VHF BPF\n");
-          txLed = 2;
-          txLedOn = HIGH;
-          txLedOff = LOW;
-          vB5 = TRUE;
-          onLed = 27;
-          onLedOn = HIGH;
-          onLedOff = LOW;
-          transmit = TRUE;
+          if (digitalRead(23) != HIGH) {
+            printf("vB5 Present with VHF BPF\n");
+            txLed = 2;
+            txLedOn = HIGH;
+            txLedOff = LOW;
+            vB5 = TRUE;
+            onLed = 27;
+            onLedOn = HIGH;
+            onLedOff = LOW;
+            transmit = TRUE;
+	  }
 	}
       }
     }
