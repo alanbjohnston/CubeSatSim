492e9343cf19a1815d9789be0e9fbc46fb77505a
alanbjohnston <alan.b.johnston@gmail.com>
2021-05-15 21:24:52 -0400

    Update rpitx.py

--- rpitx.py
+++ rpitx.py
@@ -103,5 +103,5 @@ if __name__ == "__main__":
 			system("sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3")
 	else:
     		print("No Band Pass Filter so no telemetry transmit.  See http://cubesatsim.org/wiki for instructions on how to build the BPF.") 
-    		while True:
+    		while 1:
 			time.sleep(5)
