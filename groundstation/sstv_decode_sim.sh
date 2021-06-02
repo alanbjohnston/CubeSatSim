d1d2fb193a83c42179afdc713aadaf14aad3b4e0
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-02 16:01:32 -0400

    bandwidth 96k from 48k

--- groundstation/sstv_decode_sim.sh
+++ groundstation/sstv_decode_sim.sh
@@ -35,6 +35,6 @@ qsstv &
 
 #sudo systemctl restart cubesatsim
 
-rtl_fm -M fm -f 434.9M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1 
+rtl_fm -M fm -f 434.9M -s 96k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1 
 
 $SHELL
