646f0457d7335f6d619c5dba464635a55bc8dea1
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-02 13:45:42 -0400

    added sleep(2) in place of rpitx

--- afsk/main.c
+++ afsk/main.c
@@ -1005,6 +1005,7 @@ void get_tlm(void) {
       if (transmit) {
         FILE * file2 = popen(str, "r");
         pclose(file2);
+	sleep(2);
       } else {
         fprintf(stderr, "\nNo CubeSatSim Band Pass Filter detected.  No transmissions after the CW ID.\n");
         fprintf(stderr, " See http://cubesatsim.org/wiki for info about building a CubeSatSim\n\n");
