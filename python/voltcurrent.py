0b020fef694e9838cee05b29bd7d52796e8ee326
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-03 09:04:10 -0400

    added print

--- python/voltcurrent.py
+++ python/voltcurrent.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
   if (len(sys.argv)) > 1:
 #   print("There are arguments!", file=sys.stderr)
 #   if (('a' == sys.argv[1]) or ('afsk' in sys.argv[1])):
+    print(sys.argv[1])
     buses[0] = int(sys.argv[1], base=10)
     if (len(sys.argv)) > 2:
       buses[1] = int(sys.argv[2], base=10)
