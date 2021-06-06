f1cdd20719a5e49f4e4332dc224050bad3657edf
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-06 15:25:54 -0400

    reversed changes from dev

--- update.sh
+++ update.sh
@@ -19,42 +19,24 @@ make debug
 FLAG=0
 
 if [[ $(diff systemd/cubesatsim.service /etc/systemd/system/cubesatsim.service) ]]; then
-  echo "changed cubesatsim.service"
-  sudo cp /home/pi/CubeSatSim/systemd/cubesatsim.service /etc/systemd/system/cubesatsim.service
+  echo "copying cubesatsim.service"
+  sudo cp systemd/cubesatsim.service /etc/systemd/system/cubesatsim.service
   FLAG=1
 else
   echo "no changes to cubesatsim.service"
 fi
 
 if [[ $(diff systemd/rpitx.service /etc/systemd/system/rpitx.service) ]]; then
-  echo "changed rpitx.service"
-  sudo cp /home/pi/CubeSatSim/systemd/rpitx.service /etc/systemd/system/rpitx.service
+  echo "copying rpitx.service"
+  sudo cp systemd/rpitx.service /etc/systemd/system/rpitx.service
   FLAG=1
 else
   echo "no changes to rpitx.service"
 fi
 
-FILE=/home/pi/CubeSatSim/sstv_image_1_320_x_256.jpg
-if [ ! -f "$FILE" ]; then
-    echo "Copying SSTV image 1"
-    cp /home/pi/CubeSatSim/sstv/sstv_image_1_320_x_256.jpg /home/pi/CubeSatSim/sstv_image_1_320_x_256.jpg
-fi
-    
-FILE=/home/pi/CubeSatSim/sstv_image_2_320_x_256.jpg
-if [ ! -f "$FILE" ]; then
-    echo "Copying SSTV image 2"
-    cp /home/pi/CubeSatSim/sstv/sstv_image_2_320_x_256.jpg /home/pi/CubeSatSim/sstv_image_2_320_x_256.jpg
-fi
-      
-grep 'update' /home/pi/CubeSatSim/.updated
-if [[ $(grep 'update' /home/pi/CubeSatSim/.updated) ]]; then
-  echo "update script updated"
-  /home/pi/CubeSatSim/update
-fi
-
 if [ $FLAG -eq 1 ]; then
   echo "systemctl daemon-reload and restart"
-  sudo systemctl daemon-reload 
+  sudo systemctl daemon-reload
   sudo systemctl restart cubesatsim
 else
   grep 'changed' /home/pi/CubeSatSim/.updated
