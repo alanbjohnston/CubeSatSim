294f64e8be173f070b763608f22c1d4a3d54bd3d
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-09 17:47:06 -0400

    added Tx LED for dummy APRS packets

--- rpitx.py
+++ rpitx.py
@@ -59,7 +59,9 @@ if __name__ == "__main__":
 				print("AFSK")
 				time.sleep(4)
 				for x in range(5):
+					GPIO.output(txLed, txLedOn);
 					system("sudo gen_packets -o /home/pi/CubeSatSim/telem.wav /home/pi/CubeSatSim/t.txt -r 48000 > /dev/null 2>&1 && cat /home/pi/CubeSatSim/telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3 > /dev/null 2>&1")
+					GPIO.output(txLed, txLedOff);
 					time.sleep(4)
 				while True:
 					try:
