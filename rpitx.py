123de68debf39676c34bf370ef5ab380ab3acb65
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-10 18:20:28 -0400

    added print and fixed order of sleep

--- rpitx.py
+++ rpitx.py
@@ -137,19 +137,21 @@ if __name__ == "__main__":
 		elif (mode == 'b'):
 			print("BPSK")
 			system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float &")
+			print("Turning LED on/off")
 			while 1:
 				GPIO.output(txLed, txLedOff)
+				time.sleep(0.5)
 				GPIO.output(txLed, txLedOn)
 				time.sleep(4.0)
-				time.sleep(0.5)
 		else:
 			print("FSK") 
 			system("sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3 &")
+			print("Turning LED on/off")
 			while 1:
 				GPIO.output(txLed, txLedOff)
+				time.sleep(0.5)
 				GPIO.output(txLed, txLedOn)
 				time.sleep(4.0)
-				time.sleep(0.5)
 #		else:
 #			print("FSK") 
 #			system("sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3")
