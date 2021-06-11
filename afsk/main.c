dfa8d735066f2d82c7e4bb667f86b05251c297b3
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-11 08:02:44 -0400

    typo

--- afsk/main.c
+++ afsk/main.c
@@ -1367,7 +1367,7 @@ void get_tlm_fox() {
 	int i, end, trys = 0;
 	sensor_payload[0] = 0;
 	sensor_payload[1] = 0;
-	while ((sensor_payload[0] != 'O') || (sensor_payload[1] != 'K')) && (trys++ < 10)) {	      
+	while (((sensor_payload[0] != 'O') || (sensor_payload[1] != 'K')) && (trys++ < 10)) {	      
           i = 0;
 	  serialPutchar(uart_fd, '?');
           printf("%d Querying payload with ?\n", trys);
