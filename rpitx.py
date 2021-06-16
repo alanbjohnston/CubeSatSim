74a988a70da6f76a0ca4f1465adc0f17f30f4c49
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-16 13:41:38 -0400

    tabs

--- rpitx.py
+++ rpitx.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
 	if (len(sys.argv)) > 1:
 #        	print("There are arguments!")
-        	if (('d' == sys.argv[1]) or ('-d' in sys.argv[1])):
+		if (('d' == sys.argv[1]) or ('-d' in sys.argv[1])):
 			debug_mode = 1
 			
 	print(transmit)
