13b6a2c55b37efc09c44433b92e67a8dd11efbe3
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-16 20:26:14 -0400

    back to twice every time

--- afsk/main.c
+++ afsk/main.c
@@ -2204,7 +2204,7 @@ void get_tlm_fox() {
       printf("socket send 2 %d ms bytes: %d \n\n", millis() - start, sock_ret);
     }
     
-    if ((mode == BPSK) && (firstTime == 1)) // only do first time 
+    if ((mode == BPSK)) // && (firstTime == 1)) // only do first time 
     {	  
       start = millis();  // send frame twice 
       sock_ret = send(sock, buffer, (unsigned int)(ctr * 2 + 2), 0);
@@ -2215,16 +2215,6 @@ void get_tlm_fox() {
         sleep(0.5);
         sock_ret = send(sock, &buffer[sock_ret], (unsigned int)(ctr * 2 + 2 - sock_ret), 0);
         printf("socket send 4 %d ms bytes: %d \n\n", millis() - start, sock_ret);
-      }
-       start = millis();  // send frame twice 
-      sock_ret = send(sock, buffer, (unsigned int)(ctr * 2 + 2), 0);
-      printf("socket send 5 %d ms bytes: %d \n\n", (unsigned int)millis() - start, sock_ret);
-      
-      if (sock_ret < (ctr * 2 + 2)) {
-  //    printf("Not resending\n");
-        sleep(0.5);
-        sock_ret = send(sock, &buffer[sock_ret], (unsigned int)(ctr * 2 + 2 - sock_ret), 0);
-        printf("socket send 6 %d ms bytes: %d \n\n", millis() - start, sock_ret);
       }
     }
 
