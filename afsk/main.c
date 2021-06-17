054bccfdbc6c5dccc1e938af70e90cccd8023abb
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-17 07:14:08 -0400

    SSTV sleep 50

--- afsk/main.c
+++ afsk/main.c
@@ -876,7 +876,7 @@ int main(int argc, char * argv[]) {
       get_tlm_fox();
     } else {  				// SSTV	    
       fprintf(stderr, "Sleeping\n");
-      sleep(100);	    
+      sleep(50);	    
     }
 
     #ifdef DEBUG_LOGGING
