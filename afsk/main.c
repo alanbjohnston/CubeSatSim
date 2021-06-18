11f9bffffe8bc38ffa84d9200d1a2bf5ee0b7b1e
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-18 13:35:50 -0400

    send twice all the time

--- afsk/main.c
+++ afsk/main.c
@@ -2201,7 +2201,17 @@ void get_tlm_fox() {
       sock_ret = send(sock, &buffer[sock_ret], (unsigned int)(ctr * 2 + 2 - sock_ret), 0);
       printf("socket send 2 %d ms bytes: %d \n\n", millis() - start, sock_ret);
     }
+     start = millis();
+    int sock_ret = send(sock, buffer, (unsigned int)(ctr * 2 + 2), 0);
+    printf("socket send 1a %d ms bytes: %d \n\n", (unsigned int)millis() - start, sock_ret);
+    fflush(stdout);	  
     
+    if (sock_ret < (ctr * 2 + 2)) {
+  //    printf("Not resending\n");
+      sleep(0.5);
+      sock_ret = send(sock, &buffer[sock_ret], (unsigned int)(ctr * 2 + 2 - sock_ret), 0);
+      printf("socket send 2a %d ms bytes: %d \n\n", millis() - start, sock_ret);
+    }   
 //    if ((mode == BPSK) && (firstTime == 1)) // only do first time 
     if (firstTime == 1) // only do first time 
     {
