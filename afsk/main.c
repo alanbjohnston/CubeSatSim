c1fe08c0958f4b0d1b1a3d06e36d890d882863ff
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-19 10:22:56 -0400

    back to - 200

--- afsk/main.c
+++ afsk/main.c
@@ -1410,7 +1410,7 @@ void get_tlm_fox() {
      {
 //       if ((millis() - sampleTime) < ((unsigned int)frameTime - 500))  // was 100 for FSK
           sleep(3.8); // 0.5);  // 25);  // initial period
-       while ((millis() - sampleTime) < ((unsigned int)frameTime - 100))  // was 500 and 100
+       while ((millis() - sampleTime) < ((unsigned int)frameTime - 200))  // was 500 and 100
           sleep(0.1); // 0.5);  // 25);
      }  else
      {
