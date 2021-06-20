6be0dbf3d1a4832d409bb1a41a2a4833c491948c
alanbjohnston <alan.b.johnston@gmail.com>
2021-06-19 22:48:28 -0400

    added back in frameTime sleeping for both FSK and BPSK

--- afsk/main.c
+++ afsk/main.c
@@ -1403,22 +1403,24 @@ void get_tlm_fox() {
     if (firstTime != ON) {
       // delay for sample period
 
-/*
+/**/
 //      while ((millis() - sampleTime) < (unsigned int)samplePeriod)
      int startSleep = millis();	    
      if ((millis() - sampleTime) < ((unsigned int)frameTime - 100))  // was 500 for FSK
         sleep(3.8); // 0.5);  // 25);  // initial period
      while ((millis() - sampleTime) < ((unsigned int)frameTime - 100))  // was 100
-        sleep(0.25); // 0.5);  // 25);
+        sleep(0.1); // 25); // 0.5);  // 25);
 //        sleep((unsigned int)sleepTime);
-*/
+/**/
 
+/*	    
      if ((millis() - sampleTime) < 500)    //
        if (mode == BPSK)
 	  sleep(4.0);
 //       else // FSK
      if (mode == FSK)
        sleep(2.3);	    
+*/
 /*	
       // most recent sleep code
       
