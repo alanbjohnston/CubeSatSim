e497bea07aea0644f90a371e8dad140c71b24588
Alan Johnston <alan.b.johnston@gmail.com>
2021-07-02 17:46:38 -0400

    initial add

---
+++ groundstation/fox-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# script to change FoxTelem default profile 
+
+echo "Script to change your default FoxTelem profile for FIAB v3"
+
+echo 
+
+sudo rm /home/pi/FoxTelemetryData/.foxprofile  
+
+/home/pi/CubeSatSim/groundstaton/fox-startup.sh
+
+$SHELL
