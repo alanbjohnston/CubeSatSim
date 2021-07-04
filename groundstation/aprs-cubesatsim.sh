66ba7d26bcbb170840c1429d48f33008c2f0c506
alanbjohnston <alan.b.johnston@gmail.com>
2021-07-04 18:59:41 -0400

    sleep 5

--- groundstation/aprs-cubesatsim.sh
+++ groundstation/aprs-cubesatsim.sh
@@ -3,8 +3,6 @@
 
 sudo systemctl stop openwebrx
 
-sleep 2
-
 sudo modprobe snd-aloop
 
 sudo systemctl stop rtl_tcp
@@ -27,11 +25,11 @@ sudo killall -9 CubicSDR &>/dev/null
 
 echo -e "Script to auto decode APRS packets from CubeSatSim on 434.90 MHz\n"
 
-sleep 1
-
 #sudo rtl_fm -f 144.39M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
 
 direwolf -t 0 &
+
+sleep 5
 
 #rtl_fm -M fm -f 144.39M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1
 rtl_fm -M fm -f 434.9M -s 48k | aplay -D hw:3,0,0 -r 48000 -t raw -f S16_LE -c 1
