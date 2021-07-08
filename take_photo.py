5915fc747789595f0c8106197cb873309ceb1f4f
alanbjohnston <alan.b.johnston@gmail.com>
2021-07-08 16:55:03 -0400

    fixed month

--- take_photo.py
+++ take_photo.py
@@ -7,5 +7,5 @@ camera.resolution = (2592,1944)
 camera.start_preview()
 
 current_time = time.strftime("%H;%M;%S", time.localtime())
-current_time = time.strftime("%e-%d-%H:%M:%S", time.localtime())
+current_time = time.strftime("%m-%d-%H:%M:%S", time.localtime())
 camera.capture('/home/pi/payload/img'+current_time+'.jpg')
