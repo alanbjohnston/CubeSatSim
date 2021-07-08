b20ddad1442e5e276846fb3746d6b5dbf8d06add
alanbjohnston <alan.b.johnston@gmail.com>
2021-07-08 16:29:26 -0400

    reduced resolution

--- take_photo.py
+++ take_photo.py
@@ -2,7 +2,8 @@ import time
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution = (3280,2464)
+#camera.resolution = (3280,2464)
+camera.resolution = (2592,1944)
 camera.start_preview()
 
 current_time = time.strftime("%H;%M;%S", time.localtime())
