c9ed87da7cdcc6954a32fac2ce095c1e18afa85f
Alan Johnston <alan.b.johnston@gmail.com>
2021-07-10 14:55:37 +0100

    initial add

---
+++ take-photos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\nScript to take pictures every 30 seconds\n"
+
+while true; do
+
+    python3 /home/pi/CubeSatSim/take-photo.py
+    echo -e "\nTaking photo!\n"
+    sleep 28
+
+done
