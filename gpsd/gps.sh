fac2cf106827eb4b2ba2a75e85e3194e804e33d2
alanbjohnston <alan.b.johnston@gmail.com>
2021-07-11 19:19:14 -0400

    got rid of test

--- gpsd/gps.sh
+++ gpsd/gps.sh
@@ -2,14 +2,9 @@
 
 # from https://stackoverflow.com/questions/28387230/use-gpsd-or-cgps-to-return-latitude-and-longitude-then-quit
 
-gpspipe -w -n 5 | grep DEVICES | tail -n1|cut -d":" -f5|cut -d"," -f1 | grep ttyUSB 
-if [[ $(gpspipe -w -n 5 | grep DEVICES | tail -n1|cut -d":" -f5|cut -d"," -f1 | grep ttyUSB) ]]; then
+x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f9|cut -d"," -f1)
+y=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f10|cut -d"," -f1)
+a=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f11|cut -d"," -f1)
 
-  x=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f9|cut -d"," -f1)
-  y=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f10|cut -d"," -f1)
-  a=$(gpspipe -w -n 10 |grep lon|tail -n1|cut -d":" -f11|cut -d"," -f1)
-  echo "$x $y $a"
-else
-  echo "0 0 0"
-fi
+echo "$x $y $a"
 
