d8510ab5cb0efc64141078fbbcf3ef7abe6dd0d3
alanbjohnston <alan.b.johnston@gmail.com>
2021-07-12 23:19:34 -0400

    don't set lat long if 0

--- main.c
+++ main.c
@@ -928,11 +928,12 @@ void get_tlm(void) {
   printf("GPS Data: %f, %f, %f \n", lat_gps, lon_gps, alt_gps); 
   pclose(file_gps);
 	  
-//  if (lat_gps != 0.0) {	  
+  if (lat_gps != 0.0) {	  
     latitude = toAprsFormat(lat_gps);
     longitude = toAprsFormat(lon_gps);
-    sprintf(gps_str, "%7.4f %7.4f %7.1f", lat_gps, lon_gps, alt_gps);	  
-//  }	  
+//    sprintf(gps_str, "%7.4f %7.4f %7.1f", lat_gps, lon_gps, alt_gps);	  
+  }	  
+    sprintf(gps_str, "%7.4f %7.4f %7.1f", toAprsFormat(lat_gps), tpA[rsFpr,at(lon_gps), alt_gps);	  
   	  	  
 //      printf("Str: %s \n", str);
       if (mode != CW) {
