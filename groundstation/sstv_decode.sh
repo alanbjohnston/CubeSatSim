2b93a40b3b90c210369a6fb35f914372a0ec516e
alanbjohnston <alan.b.johnston@gmail.com>
2021-08-11 10:02:02 -0400

    added tee to play audio

--- groundstation/sstv_decode.sh
+++ groundstation/sstv_decode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# script to decode SSTV using rtl_fm and QSSTV from the CubeSatSim
+# script to decode SSTV using rtl_fm and QSSTV from the C
 
 echo "Script to decode SSTV from the CubeSatSim for ARISS Radio Pi"
 
@@ -27,6 +27,7 @@ qsstv &
 
 sleep 5
 
-rtl_fm -M fm -f 434.9M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1 &
+#rtl_fm -M fm -f 434.9M -s 48k | aplay -D hw:2,0,0 -r 48000 -t raw -f S16_LE -c 1 &
+rtl_fm -M fm -f 434.9M -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
 
 $SHELL
