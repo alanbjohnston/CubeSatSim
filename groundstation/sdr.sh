50c0104c2df51236945829c6de5a94b13e5667f0
alanbjohnston <alan.b.johnston@gmail.com>
2021-08-12 08:36:20 -0400

    added killall rtl_tcp

--- groundstation/sdr.sh
+++ groundstation/sdr.sh
@@ -21,6 +21,8 @@ sudo killall -9 java &>/dev/null
 
 sudo killall -9 rtl_fm &>/dev/null
 
+sudo killall -9 rtl_tcp &>/dev/null
+
 sudo killall -9 CubicSDR &>/dev/null
 
 sudo killall -9 qsstv &>/dev/null
