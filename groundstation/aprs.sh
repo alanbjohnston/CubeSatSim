62a2b65623bb5b762379a2602c08633965596ccb
alanbjohnston <alan.b.johnston@gmail.com>
2021-08-23 22:08:52 -0400

    added frequency prompt

--- groundstation/aprs.sh
+++ groundstation/aprs.sh
@@ -23,7 +23,42 @@ sudo killall -9 java &>/dev/null
 
 sudo killall -9 CubicSDR &>/dev/null
 
-echo -e "Script to auto decode APRS packets on 144.390 MHz\n"
+echo
+
+echo "Choose the number for the frequency for APRS decoding:"
+echo "1. APRS Standard 2m US (144390Hz)"
+echo "2. CubeSatSim (434900 kHz)"
+echo "3. Enter a frequency in kiloHertz"
+echo
+
+read -r choice
+
+if [ "$choice" = "1" ]; then
+
+frequency=144390000
+
+elif [ "$choice" = "2" ]; then
+
+frequency=434900000
+
+else
+
+echo
+
+echo "Enter the frequency in kiloHertz"
+
+echo
+
+read -r frequency
+
+frequency=$frequency"000"
+
+fi
+
+echo $frequency
+
+
+echo -e "Script to auto decode APRS packets on $frequency\n"
 
 #sudo rtl_fm -f 144.39M -s 22050 -g 48 - | multimon-ng -a AFSK1200 -A -t raw -
 
@@ -36,6 +71,6 @@ echo "$value" > /dev/null
 set -- $value
 
 #rtl_fm -M fm -f 144.39M -s 48k | aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1
-rtl_fm -M fm -f 144.39M -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
+rtl_fm -M fm -f $frequency -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
 
 sleep 5
