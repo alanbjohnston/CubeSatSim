bac1f955870b0fede5b59261d9a6f45e026e1cd6
alanbjohnston <alan.b.johnston@gmail.com>
2021-08-26 08:12:35 -0400

    added prompt for 4 different packet decoding modes

--- groundstation/cubicsdr-decode.sh
+++ groundstation/cubicsdr-decode.sh
@@ -11,10 +11,31 @@ if [ ! -f "$FILE" ]; then
 
 fi
 
+FILE=direwolf-4800.conf
+if [ ! -f "$FILE" ]; then
+
+  wget https://raw.githubusercontent.com/alanbjohnston/CubeSatSim/FIAB-v3/groundstation/direwolf-4800.conf 
+
+fi
+
 FILE=cubicsdr-aprs-direwolf.xml
 if [ ! -f "$FILE" ]; then
 
   wget https://raw.githubusercontent.com/alanbjohnston/CubeSatSim/FIAB-v3/groundstation/cubicsdr/cubicsdr-aprs-direwolf.xml 
+
+fi
+
+FILE=cubicsdr-iss-direwolf.xml
+if [ ! -f "$FILE" ]; then
+
+  wget https://raw.githubusercontent.com/alanbjohnston/CubeSatSim/FIAB-v3/groundstation/cubicsdr/cubicsdr-iss-direwolf.xml 
+
+fi
+
+FILE=cubicsdr-serenity-direwolf.xml
+if [ ! -f "$FILE" ]; then
+
+  wget https://raw.githubusercontent.com/alanbjohnston/CubeSatSim/FIAB-v3/groundstation/cubicsdr/cubicsdr-serenity-direwolf.xml 
 
 fi
 
@@ -27,20 +48,14 @@ echo
 
 echo "Choose the number for the type of decoding:"
 echo
-echo "1. APRS US 2m (144390 kHz)"
-echo "2. ISS APRS (145825 kHz"
-echo "3. APRS Enter a frequency in kiloHertz"
-echo "4. Serenity CubeSat 4800 bps"
+echo "1. APRS US 2m (144.390 MHz)"
+echo "2. ISS APRS (145.825 MHz"
+echo "3. APRS on some other frequency"
+echo "4. Serenity CubeSat 4800 bps (437.1 MHz)"
 
 echo
 
 read -r choice
-
-if [ "$choice" = "1" ]; then
-
-frequency=144390000
-
-elif [ "$choice" = "2" ]; then
 
 
 echo
@@ -55,15 +70,56 @@ echo "When CubicSDR opens, select Generic RTL2832 device then click Start to beg
 
 echo
 
-echo "Then select File/Session/Open Session then load /home/pi/CubeSatSim/groundstation/cubicsdr/cubicsdr-aprs-direwolf.xml then Open."
+if [ "$choice" = "1" ]; then
 
-echo 
+  echo "Then select File/Session/Open Session then load /home/pi/CubeSatSim/groundstation/cubicsdr/cubicsdr-aprs-direwolf.xml then Open."
 
-echo "Hit the Return key when you have done this in CubicSDR and Direwolf will start"
+  echo 
 
-read input
+  echo "Hit the Return key when you have done this in CubicSDR and Direwolf will start"
 
-direwolf -c direwolf.conf -r 48000 -t 0
+  read input
+
+  direwolf -c direwolf.conf -r 48000 -t 0
+
+elif [ "$choice" = "2" ]; then
+
+  echo "Then select File/Session/Open Session then load /home/pi/CubeSatSim/groundstation/cubicsdr/cubicsdr-iss-direwolf.xml then Open."
+
+  echo 
+
+  echo "Hit the Return key when you have done this in CubicSDR and Direwolf will start"
+
+  read input
+
+  direwolf -c direwolf.conf -r 48000 -t 0
+
+elif [ "$choice" = "3" ]; then
+
+  echo "Then select File/Session/Open Session then load /home/pi/CubeSatSim/groundstation/cubicsdr/cubicsdr-aprs-direwolf.xml then Open."
+  echo "Then tune the CubicSDR frequency to the one you want to decode."
+
+  echo 
+
+  echo "Hit the Return key when you have done this in CubicSDR and Direwolf will start"
+
+  read input
+
+  direwolf -c direwolf.conf -r 48000 -t 0
+
+elif [ "$choice" = "4" ]; then
+
+  echo "Then select File/Session/Open Session then load /home/pi/CubeSatSim/groundstation/cubicsdr/cubicsdr-serenity-direwolf.xml then Open."
+
+  echo 
+
+  echo "Hit the Return key when you have done this in CubicSDR and Direwolf will start"
+
+  read input
+
+  direwolf -c direwolf-4800.conf -r 48000 -t 0
+
+fi
 
 sleep 5
 
