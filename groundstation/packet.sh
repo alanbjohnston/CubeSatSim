fd89037952adc4b0fa05e91055e8da12e42e4a82
alanbjohnston <alan.b.johnston@gmail.com>
2021-09-18 16:57:58 -0400

    changed to 1200 for APRS WAV

--- groundstation/packet.sh
+++ groundstation/packet.sh
@@ -129,7 +129,7 @@ else
   #aplay -D hw:${2:0:1},0,0 /home/pi/CubeSatSim/telem.wav &
   (while true; do (sleep 2 && aplay -D hw:${2:0:1},0,0 /home/pi/CubeSatSim/telem.wav); done) &
   
-  timeout 30 direwolf -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-4800.conf -r 48000 -t 0
+  timeout 30 direwolf -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf.conf -r 48000 -t 0
   
   echo
   
