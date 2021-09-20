5ed7a9d5c762dfabd85005a08930266905fb08c2
alanbjohnston <alan.b.johnston@gmail.com>
2021-09-19 22:42:04 -0400

    changed format

--- groundstation/loc-add-foxtelem.py
+++ groundstation/loc-add-foxtelem.py
@@ -121,7 +121,8 @@ if ((latitude != 0) and (longitude != 0) or grid):
       
       dquote = '\\"'
       
-      receiver_gpsSedStr = 'sudo sed -i "s/receiver_gps =.*/receiver_gps = {' + dquote + 'lat' + dquote + ': ' + str(latitude) + ', ' + dquote + 'lon' + dquote + ': ' + str(longitude) + '}/g" /var/lib/openwebrx/settings.json'
+ #     receiver_gpsSedStr = 'sudo sed -i "s/receiver_gps =.*/receiver_gps = {' + dquote + 'lat' + dquote + ': ' + str(latitude) + ', ' + dquote + 'lon' + dquote + ': ' + str(longitude) + '}/g" /var/lib/openwebrx/settings.json'
+      receiver_gpsSedStr = 'sudo sed -i "s/        ' + dquote + 'lat' + dquote + 'lat' + dquote + 'lat' + dquote + ': .*/        ' + dquote + 'lat' + dquote + 'lat' + dquote + 'lat' + dquote + ': ' + str(latitude) + '/g" /home/pi/settings.json'
       #print (longSedStr)
       system(receiver_gpsSedStr)
 
