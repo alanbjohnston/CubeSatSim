9c0b49a4f39798328e8f5a3dcb996d9a5d5fb8d3
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-05 16:59:18 -0400

    change window size

--- groundstation/packet.sh
+++ groundstation/packet.sh
@@ -26,7 +26,7 @@ sudo killall -9 CubicSDR &>/dev/null
 
 echo
 
-frequency=$(zenity --list 2>/dev/null --title="Choose the frequency" --column="kHz" --column="Use" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 145175 "APRS Australian 2m" Other "Choose another frequency" 145825 "APRS on ISS" 437100 "Serenity CubeSat 4800 bps" Serenity "Test Serenity CubeSat decoding with WAV file" APRS "Test APRS decoding with CubeSatSim WAV file")
+frequency=$(zenity --list 2>/dev/null --width=410 --height=360 --title="Choose the frequency for packet decoding" --column="kHz" --column="Application" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 145175 "APRS Australian 2m" Other "Choose another frequency" 145825 "APRS on ISS" 437100 "Serenity CubeSat 4800 bps" Serenity "Test Serenity CubeSat decoding with WAV file" APRS "Test APRS decoding with CubeSatSim WAV file")
 
 #echo $frequency
 
