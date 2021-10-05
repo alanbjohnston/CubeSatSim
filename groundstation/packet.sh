ae0fc8cf1a5db56be7b1a17b5c086e79a742477f
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-05 10:15:04 -0400

    fixed zenity

--- groundstation/packet.sh
+++ groundstation/packet.sh
@@ -26,7 +26,7 @@ sudo killall -9 CubicSDR &>/dev/null
 
 echo
 
-frequency=$(zenity --error 2>/dev/null --list --title="Choose the frequency" --column="kHz" --column="Use" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 2>/dev/null)"000"
+frequency=$(zenity --error 2>/dev/null --list --title="Choose the frequency" --column="kHz" --column="Use" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m")"000"
 
 if [[ $frequency -eq "000" ]]; then 
 
