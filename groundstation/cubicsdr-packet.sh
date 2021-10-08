48e827d2f92ed8b05c0eb08c6b574d71fc23c334
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-07 21:41:51 -0400

    menu changes

--- groundstation/cubicsdr-packet.sh
+++ groundstation/cubicsdr-packet.sh
@@ -24,7 +24,7 @@ sudo killall -9 CubicSDR &>/dev/null
 
 #sleep 5
 
-frequency=$(zenity --list 2>/dev/null --width=410 --height=320 --title="Packet Decoding with CubicSDR and Direwolf" --text="Choose the frequency for packet decoding" --column="kHz" --column="Application" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 145175 "APRS Australian 2m" Other "Choose another frequency" 145825 "APRS on ISS" 437100 "Serenity CubeSat 4800 bps" Serenity "Test Serenity CubeSat decoding with WAV file")
+frequency=$(zenity --list 2>/dev/null --width=510 --height=320 --title="Packet Decoding with CubicSDR and Direwolf" --text="Choose the frequency for packet decoding" --column="kHz" --column="Application" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 145175 "APRS Australian 2m" Other "Choose another frequency" 145825 "APRS on ISS" 437100 "Serenity CubeSat 4800 bps" Serenity "Test Serenity CubeSat decoding with WAV file")
 
 #echo $frequency
 
