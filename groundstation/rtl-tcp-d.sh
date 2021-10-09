d2de25a7e6c4458bd71ed76474147c2e8966a2aa
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-09 11:34:54 -0400

    added killall for popups

--- groundstation/rtl-tcp-d.sh
+++ groundstation/rtl-tcp-d.sh
@@ -29,6 +29,14 @@ sudo killall -9 qsstv &>/dev/null
 
 sudo killall -9 direwolf &>/dev/null
 
+sudo kill `ps -aux | grep cubicsdr-packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+
+sudo kill `ps -aux | grep packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+
+sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+
+sudo killall -9 zenity &>/dev/null
+
 sudo systemctl stop rtl_tcp
 
 sleep 5
