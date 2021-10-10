b25b0844216dbb9c6d0486940635d5e849bbe1f5
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-09 20:05:48 -0400

    removed script killall

--- groundstation/cubicsdr-packet.sh
+++ groundstation/cubicsdr-packet.sh
@@ -22,9 +22,9 @@ sudo killall -9 java &>/dev/null
 
 sudo killall -9 CubicSDR &>/dev/null
 
-sudo kill `ps -aux | grep "/packet" | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+#sudo kill `ps -aux | grep "/packet" | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
-sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+#sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
 sudo killall -9 zenity &>/dev/null
 
