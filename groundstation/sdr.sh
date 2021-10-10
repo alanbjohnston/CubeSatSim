d688e083a2a0078872a7497e4cbcef1fb64cd54d
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-09 20:12:34 -0400

    removed script killall

--- groundstation/sdr.sh
+++ groundstation/sdr.sh
@@ -36,11 +36,11 @@ sudo killall -9 qsstv &>/dev/null
 
 sudo killall -9 direwolf &>/dev/null
 
-sudo kill `ps -aux | grep cubicsdr-packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+#sudo kill `ps -aux | grep cubicsdr-packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
-sudo kill `ps -aux | grep packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+#sudo kill `ps -aux | grep packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
-sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+#sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
 sudo killall -9 zenity &>/dev/null
 
