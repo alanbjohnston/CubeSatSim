6f05c3e0b75123a0e2d62d215f312194c5019fc0
alanbjohnston <alan.b.johnston@gmail.com>
2021-10-11 21:31:33 -0400

    fixed killall's

--- groundstation/rtl-tcp-d.sh
+++ groundstation/rtl-tcp-d.sh
@@ -19,25 +19,25 @@ echo
 
 sudo systemctl stop openwebrx
 
+sudo systemctl stop rtl_tcp
+
+pkill -o chromium &>/dev/null
+
 sudo killall -9 java &>/dev/null
 
 sudo killall -9 rtl_fm &>/dev/null
 
 sudo killall -9 CubicSDR &>/dev/null
 
+sudo killall -9 aplay &>/dev/null
+
 sudo killall -9 qsstv &>/dev/null
 
 sudo killall -9 direwolf &>/dev/null
 
-#sudo kill `ps -aux | grep cubicsdr-packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
-
-#sudo kill `ps -aux | grep packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
-
-#sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
-
 sudo killall -9 zenity &>/dev/null
 
-sudo systemctl stop rtl_tcp
+sudo killall -9 rtl_tcp &>/dev/null
 
 sleep 5
 
