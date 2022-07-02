4461fb5b0893f9bbe71c9a739bb95d8ff463d4ba
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-02 14:47:15 -0400

    Update cubesatsim.h

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -137,6 +137,7 @@ int rd = 0;
 int nrd;
 void write_to_buffer(int i, int symbol, int val);
 void write_wave(int i, short int * buffer);
+void sleep(float time);
 int uart_fd;
 
 int reset_count = 0;
