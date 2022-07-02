571ffc193b489fbaa527c2b05f220c8de59abdda
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-02 08:05:51 -0400

    Update cubesatsim.h

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -153,3 +153,34 @@ int mapping[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 char src_addr[5] = "";
 char dest_addr[6] = "APCSS";
 float voltage_min[9], current_min[9], voltage_max[9], current_max[9], sensor_max[17], sensor_min[17], other_max[3], other_min[3];
+
+
+/*
+ * TelemEncoding.h
+ *
+ *  Created on: Feb 3, 2014
+ *      Author: fox
+ */
+
+#ifndef TELEMENCODING_H_
+#define TELEMENCODING_H_
+
+/*
+void update_rs(
+   unsigned char parity[32], // 32-byte encoder state; zero before each frame
+   unsigned char c          // Current data byte to update
+);
+*/
+
+#define CHARACTER_BITS 10
+#define CHARACTERS_PER_LONGWORD 3
+#define CHARACTER_MASK ((1<<CHARACTER_BITS)-1)
+#define SYNC_CHARACTER -1
+
+
+#define PARITY_BYTES_PER_CODEWORD 32U     // Number of parity symbols in frame
+#define NP 32U //For Phil's code
+#define DATA_BYTES_PER_CODE_WORD 223
+
+#endif /* TELEMENCODING_H_ */
+
