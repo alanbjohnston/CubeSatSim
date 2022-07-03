a7a5260b4f3c1428d8a84619bb4bf55dfc853353
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-02 23:49:41 -0400

    Update cubesatsim.h

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -27,7 +27,7 @@
 #define TXC_PIN 11 // Transceiver Board is present
 #define SWTX_PIN 14 // SR_FRS_05W Transmit Pico software serial port 
 #define SQUELCH 15 // SR_FRS_05W Squelch out
-#define MAIN_INA219  // Power the INA219s on the Main board
+#define MAIN_INA219 16 // Power the INA219s on the Main board
 #define PTT_PIN 17 // SR_FRS_05W PTT Push to Talk - transmit
 #define STEM_LED1 18 // STEM board LED1 Green
 #define STEM_LED2 19 // STEM board LED2 Blue
