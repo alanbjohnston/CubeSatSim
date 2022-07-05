3a2f4990407d0103d687b93c0bc872824c1549a1
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-05 09:49:43 -0400

    Update cubesatsim.h

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -142,8 +142,8 @@ int testCount = 0;
 long time_start;
 //char cmdbuffer[1000];
 //FILE * file1;//
-unsigned int buffer_size = 2^16;
-short int buffer[buffer_size]; // ctr is an int // 100000]; // 50000]; // 25000]; // 10240]; // was 2336400]; // max size for 10 frames count of BPSK
+#define BUFFER_SIZE  (2^16)
+short int buffer[BUFFER_SIZE]; // ctr is an int // 100000]; // 50000]; // 25000]; // 10240]; // was 2336400]; // max size for 10 frames count of BPSK
 //short int buffer[(WAV_DATA_LENGTH/8)];
 //FILE *sopen(const char *program);
 
