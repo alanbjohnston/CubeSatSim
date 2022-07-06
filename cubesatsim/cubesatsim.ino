0489fc587f01ac7780c45b0fd407e26bf3867635
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-06 10:21:14 -0400

    Update cubesatsim.ino

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -2209,11 +2209,11 @@ void pwm_interrupt_handler() {
 		
           if ((pwm_value == (128 - pwm_amplitude)) && (pwm_rnd_bit == 1)) {
             pwm_value = 128 + pwm_amplitude;
-//            Serial.print("-");
+            Serial.print("-");
           }
           else {
             pwm_value = 128 - pwm_amplitude; 
-//            Serial.print("_");
+            Serial.print("_");
           }
 	  pwm_set_gpio_level(AUDIO_OUT_PIN, pwm_value);
 //	  Serial.println("wav_position: ");
