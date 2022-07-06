9147e0955baf9e41eda3d3024bf591506cbd8486
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-06 11:36:16 -0400

    Update cubesatsim.h

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -243,7 +243,7 @@ float rest;
 unsigned int wav_position = 0;
 int pwm_counter = 0;
 int pwm_counter_max = 420;
-int pwm_amplitude = 5; //50 //100;
+int pwm_amplitude = 50; //50 //100;
 int pwm_value;
 int pwm_rnd_bit = 1;
 /*
