1afb378d903a2494c68ab064d0d6daa1881fba3f
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-09 15:00:56 -0400

    Update cubesatsim.ino

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -1698,7 +1698,7 @@ void config_radio()
 //    mySerial.println("AT+DMOSETGROUP=0,434.9000,434.9000,1,2,1,1\r");    
    mySerial.println("AT+DMOSETGROUP=0,434.9100,434.9100,0,8,0,0\r");  
    sleep(0.1);	  
-   mySerial.println("AT+DMOSETMIC=4,0\r");  
+   mySerial.println("AT+DMOSETMIC=1,0\r");  
   }
 	
   if (mode == FSK)	  
