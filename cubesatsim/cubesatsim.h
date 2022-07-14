bee5e4eb7b2622fb9df016b32730774193379888
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-14 18:51:45 -0400

    changed back to i2c_bus 0 to 3

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -209,7 +209,7 @@ double cpuTemp;
 int frameTime;
 
 float axis[3], angle[3], volts_max[3], amps_max[3], batt, speed, period, tempS, temp_max, temp_min, eclipse;
-int i2c_bus1 = OFF, i2c_bus2 = OFF, camera = OFF, sim_mode = FALSE, SafeMode = FALSE, rxAntennaDeployed = 0, txAntennaDeployed = 0;
+int i2c_bus0 = OFF, i2c_bus1 = OFF, i2c_bus3 = OFF, camera = OFF, sim_mode = FALSE, SafeMode = FALSE, rxAntennaDeployed = 0, txAntennaDeployed = 0;
 double eclipse_time;
 
 float voltage[9], current[9], sensor[17], other[3];
