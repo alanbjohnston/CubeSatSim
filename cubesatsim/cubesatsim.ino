f143a37013de089f059d62c5f3d9eabcd855a267
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-23 08:21:25 -0400

    Pico

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -52,7 +52,7 @@ char payload_str[100];
 WiFiServer server(port);
 WiFiClient client;
 
-#define PICO_W    // define if Pico W board.  Otherwise, compilation fail for Pico or runtime fail if compile as Pico W
+//#define PICO_W    // define if Pico W board.  Otherwise, compilation fail for Pico or runtime fail if compile as Pico W
 
 void setup() {
   new_mode = mode;
