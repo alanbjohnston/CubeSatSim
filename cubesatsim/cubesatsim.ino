fd527d40bcfa1a89985303d402c9a6250f60e0b9
alanbjohnston <alan.b.johnston@gmail.com>
2022-07-26 13:15:47 -0400

    skip PI3.3V test

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -66,7 +66,7 @@ void setup() {
 
   config_gpio();		
 	
-/**/	
+/*	
   pinMode(PI_3V3_PIN, INPUT); 	
   Serial.print("Pi 3.3V: ");
   Serial.println(digitalRead(PI_3V3_PIN));
@@ -77,7 +77,9 @@ void setup() {
       payload_OK_only();    
     }
   }
-/**/	
+*/
+  start_payload();  // above code not working, so forcing it
+	
   read_reset_count();	
 
 //  mode = FSK; // AFSK;		
