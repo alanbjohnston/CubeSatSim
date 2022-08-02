68d9860ed482127a07cd51bd8810671c53aacfc3
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-02 12:15:45 -0400

    removed 1

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3193,7 +3193,7 @@ bool TimerHandler0(struct repeating_timer *t) {
   }
   } 
   } else if (mode == SSTV)
-	  sstv_TimerHandler1(1);	  
+	  sstv_TimerHandler1();	  
 //  }
 /*	
   if (digitalRead(MAIN_PB_PIN) == PRESSED) // pushbutton is pressed
