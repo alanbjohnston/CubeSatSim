f0dd659c8979ea0034d85df06fd48f3808903afa
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-19 09:01:24 -0400

    print sleep time in transmit_cw

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3478,7 +3478,8 @@ void transmit_cw(int freq, float duration) {  // freq in Hz, duration in millise
   bool phase = HIGH;	
   while((micros() - start) < duration_us)  {
     digitalWrite(AUDIO_OUT_PIN, phase);    // ToDo: if no TXC, just turn on PWM carrier
-    phase = !phase;	 
+    phase = !phase;
+    Serial.print(min(start + duration_us - micros(), period_us) / 1.0E6);	  
     sleep(min(start + duration_us - micros(), period_us) / 1.0E6);  
     Serial.print("@");	  
   }
