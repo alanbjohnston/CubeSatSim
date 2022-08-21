a96afe45a22b456ecbada4f6291002abaf815e44
alanbjohnston <alan.b.johnston@gmail.com>
2022-08-20 22:13:26 -0400

    Update and rename pico-get-jpeg-serial.ino to pico-get-jpeg-serial.cpp

--- cubesatsim/pico-get-jpeg-serial/pico-get-jpeg-serial.cpp
+++ cubesatsim/pico-get-jpeg-serial/pico-get-jpeg-serial.cpp
@@ -1,24 +1,6 @@
-#include <LittleFS.h>
-#include <FastCRC.h>
+#include "pico-get-jpeg-serial.h"
 
-bool finished = false;
-
-char buffer[100001];
-int index1 = 0;
-char start_flag[] = "3d99de816e5ad7742b61a37c39141783";
-char end_flag[] = "f681a5c52351befe0e3524eb1a40f14b7803317a";
-int flag_count = 0;
-int start_flag_detected = false;
-int start_flag_complete = false;
-int end_flag_detected = false;
-int jpeg_start = 0;
-FastCRC8 CRC8;
-
-//#define DEBUG
-
-#define PICOW true
-int led_pin = LED_BUILTIN;
-
+/*
 void setup() {
  
   // put your setup code here, to run once:
@@ -75,6 +57,7 @@ void show_dir() {
   }
   Serial.println(">");
 }
+*/
 
 void write_jpg() {
 /*
@@ -119,6 +102,7 @@ void write_jpg() {
  */      
 }
 
+/*
 void loop() {
 
   char input_file[] = "/cam.jpg"; 
@@ -131,7 +115,7 @@ void loop() {
   delay(1000);
 
 }
-
+*/
 
 void get_image_file()  {
   finished = false;
