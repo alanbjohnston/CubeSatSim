5cf06f923a7ea1d73339f71fdee36b41c2f39812
alanbjohnston <alan.b.johnston@gmail.com>
2022-09-07 16:25:18 -0400

    first time no rotate

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -224,14 +224,18 @@ void loop() {
 //      send_sstv(image_file);
 //      LittleFS.remove("/cam.bin");	  
       show_dir();	  
-      char output_file2[] = "/cam2.bin"; 	  
-      jpeg_decode(image_file, output_file2, true); // debug_mode);
-      show_dir();	  
-      char telem_display[] = " BATT:    STATUS:   TEMP:  ";	  
-      char output_file[] = "/cam.bin"; 
-      digitalWrite(PTT_PIN, HIGH);  // shouldn't need this but
-      if (!first_time_sstv)	  
+      char output_file2[] = "/cam2.bin"; 
+      if (!first_time_sstv)  {	  
+        jpeg_decode(image_file, output_file2, true); // debug_mode);
+        show_dir();	  
+        char telem_display[] = " BATT:    STATUS:   TEMP:  ";	  
+        char output_file[] = "/cam.bin"; 
+        digitalWrite(PTT_PIN, HIGH);  // shouldn't need this but  
         rotate_image(output_file2, output_file, telem_display);	
+      } else {
+       jpeg_decode(image_file, output_file, true); // debug_mode);
+       Serial.println("Don't rotate image");	  
+      }
       first_time_sstv = false;	  
       show_dir();
 	  
