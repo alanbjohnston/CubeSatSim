2a37b56ce1e40aa8e9f8aafa2640a08e3f716c51
alanbjohnston <alan.b.johnston@gmail.com>
2022-09-08 16:01:03 -0400

    back to 400

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3528,7 +3528,7 @@ void start_isr() {
 	
 //  if (ITimer0.attachInterruptInterval(833, TimerHandler0))	
 //  if (ITimer0.attachInterruptInterval(804, TimerHandler0))	
-    if (ITimer0.attachInterruptInterval(100, TimerHandler0))	// was 800 // was was 828 (841) and 828
+    if (ITimer0.attachInterruptInterval(400, TimerHandler0))	// was 800 // was was 828 (841) and 828
 //  if (ITimer0.attachInterruptInterval(1667, TimerHandler0))
     {
       if (debug_mode) 	    
