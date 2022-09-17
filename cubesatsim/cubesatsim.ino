f70b54e510844242bbdc878fce0b0ee5f92fecaf
alanbjohnston <alan.b.johnston@gmail.com>
2022-09-17 19:50:56 -0400

    FM freq to 434.9

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -4239,9 +4239,9 @@ void program_radio() {
 //    mySerial.println("AT+DMOSETGROUP=0,434.9000,434.9000,1,2,1,1\r");    
 //     mySerial.println("AT+DMOSETGROUP=0,434.9000,434.9000,0,8,0,0\r");  
 //     mySerial.println("AT+DMOSETGROUP=0,432.2510,432.2510,0,8,0,0\r");  
-     mySerial.println("AT+DMOSETGROUP=0,432.2500,432.2500,0,8,0,0\r");  
+     mySerial.println("AT+DMOSETGROUP=0,434.9000,434.9000,0,8,0,0\r");  
 //   sleep(0.5);	  
-   mySerial.println("AT+DMOSETMIC=6,0\r");  // was 8
+   mySerial.println("AT+DMOSETMIC=8,0\r");  // was 8
 	
   }
 }
