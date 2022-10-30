5e4e326f9b96d079a16a55d28e43cd13a32b5d8b
alanbjohnston <alan.b.johnston@gmail.com>
2022-10-30 10:28:40 -0400

    added print_file_hex and print_hex2

--- cubesatsim/cubesatsim.h
+++ cubesatsim/cubesatsim.h
@@ -209,7 +209,8 @@ void write_config_file();
 void read_mode(); 
 void write_mode(); 
 void start_clockgen();
-
+void print_file_hex((char* filename, int bytes);
+void print_hex2(byte octet);                    
 
 #ifndef STASSID
 #define STASSID "Pico"
