79b4fb7aa2ae676d7a1ff0183023aa2fd4ce90c8
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-12 16:35:46 -0500

    back to sleep

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3841,8 +3841,8 @@ void transmit_cw(int freq, float duration) {  // freq in Hz, duration in millise
   digitalWrite(LED_BUILTIN, HIGH);	// Transmit LED on
   digitalWrite(MAIN_LED_BLUE, HIGH);	
   dds_setfreq(freq);
-//  sleep(duration);
-  delayMicroseconds((unsigned int)(duration/1000.0));
+  sleep(duration/1000.0);
+//  delayMicroseconds((unsigned int)(duration/1000.0));
 /*	
   unsigned long start = micros();
   unsigned long duration_us = duration * 1000;
