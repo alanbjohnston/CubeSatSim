1446fd5309d6c489a521a594ec3ac4e6b35b5728
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-14 16:47:44 -0500

    add no print for 0

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -4484,7 +4484,7 @@ void print_file_hex(char* filename, int bytes) {
  //   print_hex2(buff[1]);
  //   print_hex2(buff[2]);
 	  
-          if ((buff[0]!=0xff) || (buff[1]!=0xff) || (buff[2]!=0xff))
+          if (((buff[0]!=0xff) || (buff[1]!=0xff) || (buff[2]!=0xff)) &&  ((buff[0]!=0) || (buff[1]!=0) || (buff[2]!=0)))
             Serial.printf("3Non white pixel at %d, %d R: %d G: %d B: %d\n", line, i, buff[0], buff[1],buff[2]);	  
     count += 3;	  
     if (i++ > 319)
