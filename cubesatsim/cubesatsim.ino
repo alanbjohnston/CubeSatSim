250dc4840b6454215f1ccdf88028fc14c99d6c97
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-18 15:40:06 -0500

    button_isr off, start_isr on

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -147,7 +147,7 @@ void setup() {
      configure_wifi();	  
   }
 */	
-  start_button_isr(); 
+//  start_button_isr(); 
 	
 //  setup_sstv();
   picosstvpp_begin(26);
@@ -166,7 +166,7 @@ void setup() {
 	
   config_telem();	
 	
-//  start_isr();	
+  start_isr();	
 	
 // setup radio depending on mode 
   config_radio();	
