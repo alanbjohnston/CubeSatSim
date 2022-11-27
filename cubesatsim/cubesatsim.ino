567f84250221678db466f505d0a8091c2f0fcacf
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-27 08:17:42 -0500

    start in BPSK

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -81,7 +81,9 @@ void setup() {
   LittleFS.begin();
 //  LittleFS.format();	// only format if files of size 0 keep showing up
 
-  read_mode();	
+  read_mode();
+	
+  mode = BPSK;	// force to BPSK
 
   new_mode = mode;
 	
