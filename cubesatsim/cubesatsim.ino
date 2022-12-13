31e39837c75e6c3cda66c5ce521c536c4d4538e6
alanbjohnston <alan.b.johnston@gmail.com>
2022-12-13 10:28:25 -0500

    force filter and sr_frs

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3630,16 +3630,16 @@ void config_gpio() {
   pinMode(BPF_PIN, INPUT_PULLUP);  // Read LPF to see if present
 //  pinMode(SQUELCH, INPUT);	// Squelch from TXC
 
-  if (digitalRead(BPF_PIN) == FALSE) {  
-//  if (true) {  // force BPF present
+//  if (digitalRead(BPF_PIN) == FALSE) {  
+  if (true) {  // force BPF present
     Serial.println("BPF present - transmit enabled");
     filter_present = true;  
   }
   else
     Serial.println("BPF not present - no transmitting after CW ID");	 
 	
-  if (digitalRead(TXC_PIN) == FALSE) {	
-//  if (true) {   // force SR_FRS not present
+//  if (digitalRead(TXC_PIN) == FALSE) {	
+  if (true) {   // force SR_FRS not present
     Serial.println("SR_FRS present");
     sr_frs_present = true;
   }
