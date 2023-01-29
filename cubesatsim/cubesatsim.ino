9c2768dd210c8842383c037d5c0e395dc053d515
alanbjohnston <alan.b.johnston@gmail.com>
2023-01-29 12:01:30 -0500

    no pico led blink at start

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3648,8 +3648,8 @@ void config_gpio() {
   // set LEDs and blink once	
 //  if (!wifi) 
   Serial.println("Blinking pins");	
-  pinMode(led_builtin_pin, OUTPUT);  // Set LED pin to output
-  blink_pin(led_builtin_pin, 150);	
+//  pinMode(led_builtin_pin, OUTPUT);  // Set LED pin to output
+//  blink_pin(led_builtin_pin, 150);	
 //  digitalWrite(led_builtin_pin, HIGH); // Leave Pico LED on
   pinMode(MAIN_LED_GREEN, OUTPUT);  // Set Main Green LED pin to output
   blink_pin(MAIN_LED_GREEN, 150);
