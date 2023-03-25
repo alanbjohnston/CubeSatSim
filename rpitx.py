4a61069889f7165d2f2ad4da8f508280fc2af13c
alanbjohnston <alan.b.johnston@gmail.com>
2023-03-25 11:08:01 -0400

    carrier only 0.1

--- rpitx.py
+++ rpitx.py
@@ -116,6 +116,8 @@ if __name__ == "__main__":
 		output(txLed, txLedOff)
 		output (ptt, 1)
 		sleep(1)
+	else:
+		print("Don't transmit CW ID for APRS")
 
 	if (transmit):
 	
@@ -145,7 +147,7 @@ if __name__ == "__main__":
 #					else:
 #						system("gen_packets -o /home/pi/CubeSatSim/telem.wav /home/pi/CubeSatSim/t.txt -r 48000 > /dev/null 2>&1 && cat /home/pi/CubeSatSim/telem.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3 > /dev/null 2>&1")
 					system("aplay /home/pi/CubeSatSim/telem.wav")
-					sleep(1.0)
+					sleep(0.1)
 					output (ptt, 1)
 					output(txLed, txLedOff)
 					f.close()
@@ -165,7 +167,7 @@ if __name__ == "__main__":
 				system("echo 'hi hi 100 199 199 199 298 299 299 278 380 350 300 300 439 400 400 400 500 500 500 500 600 600 600 650' > /home/pi/CubeSatSim/cw.txt")
 			output(txLed, txLedOn)
 			output (ptt, 0)
-			sleep(0.2)
+			sleep(0.1)
 #			if (debug_mode == 1):
 #				system("gen_packets -M 20 -o /home/pi/CubeSatSim/morse.wav /home/pi/CubeSatSim/cw.txt -r 48000 > /dev/null 2>&1 && cat /home/pi/CubeSatSim/morse.wav | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3")
 #			else:
