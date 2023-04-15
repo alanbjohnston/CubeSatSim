0a9f6ca9c1640472f9eeb0766f8ebb8bde619fca
alanbjohnston <alan.b.johnston@gmail.com>
2023-04-15 14:26:14 -0400

    test with SR_FRS not present

--- cubesatsim/cubesatsim.ino
+++ cubesatsim/cubesatsim.ino
@@ -3778,8 +3778,8 @@ void config_gpio() {
   else
     Serial.println("BPF not present - no transmitting after CW ID");	 
 	
-  if (digitalRead(TXC_PIN) == FALSE) {	
-//  if (true) {   // force SR_FRS not present
+//  if (digitalRead(TXC_PIN) == FALSE) {	
+  if (true) {   // force SR_FRS not present
     Serial.println("SR_FRS present");
     sr_frs_present = true;
   }
