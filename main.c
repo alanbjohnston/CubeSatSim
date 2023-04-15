ad69b44347734c475a3ad4114513415f3b9e434e
alanbjohnston <alan.b.johnston@gmail.com>
2023-04-15 09:12:11 -0400

    random 2 digit divide by 100

--- main.c
+++ main.c
@@ -764,8 +764,8 @@ int main(int argc, char * argv[]) {
 		; //payload = OFF;  // turn off since STEM Payload is not responding
       }
       if ((millis() - newGpsTime) > 60000) {
-		longitude += rnd_float(-0.005, 0.005);  // was .05
-     		latitude += rnd_float(-0.005, 0.005);	      
+		longitude += rnd_float(-0.05, 0.05) / 100.0;  // was .05
+     		latitude += rnd_float(-0.05, 0.05) / 100.0;	      
        		printf("GPS Location with Rnd: %f, %f \n", latitude, longitude);    
 	        printf("GPS Location with Rnd: APRS %7.2f, %08.2f \n", toAprsFormat(latitude), toAprsFormat(longitude));    
 	      	newGpsTime = millis();  
