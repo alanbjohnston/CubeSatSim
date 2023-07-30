98f5d40e849f36396b29ed86363ee6f8282dba5d
Alan Johnston <alan.b.johnston@gmail.com>
2023-07-30 11:41:55 -0400

    turn on FM rx for FSK and BPSK modes

--- rpitx.py
+++ rpitx.py
@@ -371,29 +371,37 @@ if __name__ == "__main__":
 						sleep(1)
 		elif (mode == 'b'):
 			print("BPSK")
+			print("turn on FM rx")
+			output(pd, 1)
+			output(ptt, 1)
 			system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float &")
-			print("Turning LED on/off")
+			print("Turning LED on/off and listening for carrier")
 			while 1:
 				output(txLed, txLedOff)
 				sleep(0.5)
 				if GPIO.input(squelch) == False:
+					print("carrier received!")
 					command_tx = ~command_tx
 					print(command_tx)
 				output(txLed, txLedOn)
 				sleep(4.0)
 		else:
 			print("FSK") 
+			print("turn on FM rx")
+			output(pd, 1)
+			output(ptt, 1)			
 			system("sudo nc -l 8080 | csdr convert_i16_f | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 | sudo /home/pi/rpitx/rpitx -i- -m RF -f 434.9e3 &")
-			print("Turning LED on/off")
+			print("Turning LED on/off and listening for carrier")
 			while 1:
 				output(txLed, txLedOff)
 				sleep(0.5)
 				if GPIO.input(squelch) == False:
+					print("carrier received!")
 					command_tx = ~command_tx
 					print(command_tx)				
 				output(txLed, txLedOn)
 				sleep(4.0)
 	else:
-		print("No Band Pass Filter so no telemetry transmit.  See http://cubesatsim.org/wiki for instructions on how to build the BPF.")
+		print("No Low Pass Filter so no telemetry transmit.  See http://cubesatsim.org/wiki for instructions on how to build the BPF.")
 		while 1:
 			sleep(5)
