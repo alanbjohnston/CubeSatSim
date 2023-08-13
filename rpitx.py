7d4305b40e51b034be496fced4825a094345ea09
Alan Johnston <alan.b.johnston@gmail.com>
2023-08-13 09:21:56 -0400

    Update rpitx.py typo

--- rpitx.py
+++ rpitx.py
@@ -547,7 +547,7 @@ if __name__ == "__main__":
 						system("echo > command_tx True")
 						system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float &")
 					else:
-						print("Turning of transmit and rebooting")
+						print("Turning off transmit") # and rebooting")
 						system("echo > command_tx False")
 						system("sudo systemctl restart rpitx")
 #						system("sudo reboot now")
