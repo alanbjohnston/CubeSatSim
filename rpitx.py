46f77e02adaf26b0024c7d7ffcb0978700f1a689
Alan Johnston <alan.b.johnston@gmail.com>
2023-08-13 09:17:55 -0400

    added file close

--- rpitx.py
+++ rpitx.py
@@ -578,7 +578,10 @@ if __name__ == "__main__":
 					try:
 						command_count += 1
 						filec = open("/home/pi/CubeSatSim/command_count.txt", "w")
-						string = filec.write(str(command_count))
+						command_count_string = str(command_count)
+						print(command_count_string)
+						string = filec.write(command_count_string)
+						filec.close()
 					except:
 						if (debug_mode == 1):
 							print("Can't write command_count file!")
