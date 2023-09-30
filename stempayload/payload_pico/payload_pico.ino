a347b6327ac417e125b93db181eaaea7d2acf782
Alan Johnston <alan.b.johnston@gmail.com>
2023-09-30 16:53:46 -0400

    Update payload_pico.ino serial read

--- stempayload/payload_pico/payload_pico.ino
+++ stempayload/payload_pico/payload_pico.ino
@@ -369,7 +369,7 @@ void payload_OK_only()
 {
   payload_str[0] = '\0';  // clear the payload string
 
-  if (Serial.available()) 
+  if (Serial.available() > 0) 
   {
     char result = Serial.read();
     Serial.println(result);	  
