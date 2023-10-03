746f0987950d6bdf010084c426bea6a70f5b50cd
Alan Johnston <alan.b.johnston@gmail.com>
2023-10-02 21:50:52 -0400

    Update main.c add program_radio at start

--- main.c
+++ main.c
@@ -44,6 +44,8 @@ int main(int argc, char * argv[]) {
   }
 	
   printf("\n\nCubeSatSim v1.3b starting...\n\n");
+
+  program_radio();	
 	
   FILE * rpitx_stop = popen("sudo systemctl stop rpitx", "r");
   pclose(rpitx_stop);
