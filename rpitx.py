a807ff0a0693906e26260beadceb63c039f72a70
Alan Johnston <alan.b.johnston@gmail.com>
2023-10-04 22:12:09 -0400

    Update rpitx.py CW ID for aprs back in

--- rpitx.py
+++ rpitx.py
@@ -262,7 +262,8 @@ if __name__ == "__main__":
 	
 	system("echo 'hi hi de " + callsign + "' > id.txt && gen_packets -M 20 /home/pi/CubeSatSim/id.txt -o /home/pi/CubeSatSim/morse.wav -r 48000 > /dev/null 2>&1")	
 	
-	if (mode != 'a') and (command_tx == True):	
+#	if (mode != 'a') and (command_tx == True):	
+	if (command_tx == True):	
 		output(pd, 1)
 		output (ptt, 0)
 		output(txLed, txLedOn)
