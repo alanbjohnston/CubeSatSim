c41932c9b41a22de2bf483379f21f084d1d5666e
Alan Johnston <alan.b.johnston@gmail.com>
2023-10-14 09:18:03 -0400

    Update rpitx.py changed to .close

--- rpitx.py
+++ rpitx.py
@@ -13,7 +13,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 def battery_saver_check():
 	try:
 		f = open("/home/pi/CubeSatSim/battery_saver")
-#		close(f)
+		f.close()
 		txc = False
 		print("battery saver activated")
 	except:
