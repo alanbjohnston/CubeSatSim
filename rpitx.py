2ff39ec7f7984c2bc8addcdc50ee5dd17f7b8bcf
Alan Johnston <alan.b.johnston@gmail.com>
2023-11-14 09:05:10 -0500

    Update rpitx.py change to bytes

--- rpitx.py
+++ rpitx.py
@@ -294,7 +294,7 @@ if __name__ == "__main__":
 	ser = serial.Serial("/dev/ttyAMA0", 9600)
 	print(ser.portstr)
 	for i in range(6):
-		ser.write("AT+DMOSETGROUP=0,435.0000,434.9100,0,3,0,0\r\n")
+		ser.write(b"AT+DMOSETGROUP=0,435.0000,434.9100,0,3,0,0\r\n")
 		sleep(0.1)
 	ser.close()
 
