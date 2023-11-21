8581a490b0d9ff19f88b0decfe915766ecb89683
Alan Johnston <alan.b.johnston@gmail.com>
2023-11-21 14:29:54 -0500

    Update main.c don't change config.txt

--- main.c
+++ main.c
@@ -2316,8 +2316,8 @@ if (setting == ON) {
 			FILE *command = popen("touch /home/pi/CubeSatSim/battery_saver", "r");
 		  	pclose(command);
 			fprintf(stderr,"Turning Battery saver mode ON\n");  
-			command = popen("if ! grep -q force_turbo=1 /boot/config.txt ; then sudo sh -c 'echo force_turbo=1 >> /boot/config.txt'; fi", "r");
-		  	pclose(command);
+//			command = popen("if ! grep -q force_turbo=1 /boot/config.txt ; then sudo sh -c 'echo force_turbo=1 >> /boot/config.txt'; fi", "r");
+//		  	pclose(command);
 			command = popen("sudo reboot now", "r");
 		  	pclose(command);
 			sleep(60);
@@ -2331,8 +2331,8 @@ if (setting == ON) {
 			FILE *command = popen("rm /home/pi/CubeSatSim/battery_saver", "r");
 		  	pclose(command);
 			fprintf(stderr,"Turning Battery saver mode OFF\n"); 
-			command = popen("sudo sed -i ':a;N;$!ba;s/\'$'\n''force_turbo=1//g' /boot/config.txt", "r");
-		  	pclose(command);
+//			command = popen("sudo sed -i ':a;N;$!ba;s/\'$'\n''force_turbo=1//g' /boot/config.txt", "r");
+//		  	pclose(command);
 			command = popen("sudo reboot now", "r");
 		  	pclose(command);
 			sleep(60);
