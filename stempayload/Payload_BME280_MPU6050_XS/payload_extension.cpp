190c60ca9931ba09617ec4f83347c8033d9f637c
Alan Johnston <alan.b.johnston@gmail.com>
2024-01-06 03:20:31 -0500

    Create payload_extension.cpp with blank sensor info

---
+++ stempayload/Payload_BME280_MPU6050_XS/payload_extension.cpp
@@ -0,0 +1,19 @@
+// Use this template for adding additional sensors
+
+// put your library includes here
+#include "Arduino.h"
+
+// put your globals here
+
+// put your setup code here
+void payload_setup() {
+  
+  Serial.println("Starting new sensor!");
+
+}
+
+// put your loop code here
+// Very Important: only use print, not println!!
+void payload_loop() {
+
+}
