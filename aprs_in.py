7f73b5d0965d00d6f2eadfc4b51f4e9cbcc56c3a
Alan Johnston <alan.b.johnston@gmail.com>
2024-05-31 15:23:31 -0400

    Update aprs_in.py remove try/except

--- aprs_in.py
+++ aprs_in.py
@@ -8,37 +8,36 @@ if __name__ == "__main__":
 #        	print("There are arguments!")
 		if ('d' == sys.argv[1]):
 			debug_mode = 1
-	try:
-		for line in sys.stdin:
-			if (debug_mode):
-				print(line)
-				
+			
+	for line in sys.stdin:
+		if (debug_mode):
+			print(line)
+			
 #			if '^c' == line.rstrip():
 #				break
-	
-			if (line.find("MODE=a")) > 0:
-				system("echo 'APRS Mode!!'")
-				mode = 'a'
-				change_mode = True
-			if (line.find("MODE=f")) > 0:
-				system("echo 'FSK Mode!!'")
-				mode = 'f'
-				change_mode = True
-			if (line.find("MODE=b")) > 0:
-				system("echo 'BPSK Mode!!'")
-				mode = 'b'
-				change_mode = True
-			if (line.find("MODE=s")) > 0:
-				system("echo 'SSTV Mode!!'")
-				mode = 's'
-				change_mode = True
-			if (line.find("MODE=m")) > 0:
-				system("echo 'CW Mode!!'")
-				mode = 'm'
-				change_mode = True
-			if (debug_mode == True)  and (change_mode == True):
-				print("/home/pi/CubeSatSim/config -" + mode)
-				system("/home/pi/CubeSatSim/config -" + mode)
-				change_mode = False
-	except:	
-		print("Done")
+
+		if (line.find("MODE=a")) > 0:
+			system("echo 'APRS Mode!!'")
+			mode = 'a'
+			change_mode = True
+		if (line.find("MODE=f")) > 0:
+			system("echo 'FSK Mode!!'")
+			mode = 'f'
+			change_mode = True
+		if (line.find("MODE=b")) > 0:
+			system("echo 'BPSK Mode!!'")
+			mode = 'b'
+			change_mode = True
+		if (line.find("MODE=s")) > 0:
+			system("echo 'SSTV Mode!!'")
+			mode = 's'
+			change_mode = True
+		if (line.find("MODE=m")) > 0:
+			system("echo 'CW Mode!!'")
+			mode = 'm'
+			change_mode = True
+		if (debug_mode == True)  and (change_mode == True):
+			print("/home/pi/CubeSatSim/config -" + mode)
+			system("/home/pi/CubeSatSim/config -" + mode)
+			change_mode = False
+	print("Done")
