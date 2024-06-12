9de07285812d0eb3a420df6e7c0ee70c8efaf30f
Alan Johnston <alan.b.johnston@gmail.com>
2024-06-12 08:00:39 -0400

    Update main.c changed clock to GPIO 6 pin 31

--- main.c
+++ main.c
@@ -102,7 +102,7 @@ int main(int argc, char * argv[]) {
   pclose(gpiod);
   gpiod = popen("pigs m 19 5", "r");		// set PWM audio utput to GPIO 19 (pin 35)
   pclose(gpiod);	
-  gpiod = popen("pigs hc 13 12880000", "r");		// set clock to 12.88 MHz on GPIO 13 (pin 33)
+  gpiod = popen("pigs hc 6 12880000", "r");		// set clock to 12.88 MHz on GPIO 6 (pin 31)
   pclose(gpiod);	
 	
 //  FILE * rpitx_stop = popen("sudo systemctl stop rpitx", "r");
