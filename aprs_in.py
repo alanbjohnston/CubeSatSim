45b18c5050a9b1e356f732e0260bcd3bf7b41581
Alan Johnston <alan.b.johnston@gmail.com>
2024-06-30 11:58:07 -0400

    Update aprs_in.py increment command_count

--- aprs_in.py
+++ aprs_in.py
@@ -38,6 +38,22 @@ if __name__ == "__main__":
 			mode = 'm'
 			change_mode = True
 		if (debug_mode == False)  and (change_mode == True):
+			try:
+				file = open("/home/pi/CubeSatSim/command_count.txt", "r")
+				string = file.read()
+				file.close()
+				command_count = int(string)
+				command_count += 1
+				filec = open("/home/pi/CubeSatSim/command_count.txt", "w")
+				command_count_string = str(command_count)
+				print(command_count_string)
+				string = filec.write(command_count_string)
+				filec.close()
+			except:
+				print("Can't write command_count file!")
+			print("Command_count: ")
+			print(command_count)							
+
 			print("\n/home/pi/CubeSatSim/config -" + mode)
 			system("/home/pi/CubeSatSim/config -" + mode)
 			change_mode = False
