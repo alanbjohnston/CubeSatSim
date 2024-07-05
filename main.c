9ce364ed7b9c6c67ab724751aafd9407cbcec2e9
Alan Johnston <alan.b.johnston@gmail.com>
2024-07-05 17:21:33 -0400

    Update main.c get payload

--- main.c
+++ main.c
@@ -381,7 +381,10 @@ int main(int argc, char * argv[]) {
 
     if ((uart_fd = serialOpen("/dev/ttyAMA0", 115200)) >= 0) {  // was 9600
       printf("Serial opened to Pico\n");	    
-//      payload = ON;	    
+//      payload = ON;	
+      payload = get_payload_serial(FALSE); 
+      printf("get_payload_status: %d \n", payload);  // not debug	    
+	    
     } else {
       fprintf(stderr, "Unable to open UART: %s\n -> Did you configure /boot/config.txt and /boot/cmdline.txt?\n", strerror(errno));
     }
@@ -614,7 +617,7 @@ int main(int argc, char * argv[]) {
 //      if ((payload == ON) && (mode != BPSK)) {  // moved to here
 //        STEMBoardFailure = 0;
 	payload = get_payload_serial(FALSE);      
-//        printf("get_payload_status: %d \n", payload);  // not debug
+        printf("get_payload_status: %d \n", payload);  // not debug
 	fflush(stdout); 
 //	printf("String: %s\n", buffer2);       
 	fflush(stdout);   
