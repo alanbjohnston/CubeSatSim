5ad71d87f5fea95515aab61d680929dae152f4ba
Alan Johnston <alan.b.johnston@gmail.com>
2024-10-14 17:17:17 -0400

    Update telem.c changed BUS to BAT2

--- telem.c
+++ telem.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[]) {
   printf("-Y   | % 4.2f V % 5.0f mA \n", voltage[map[MINUS_Y]], current[map[MINUS_Y]]);
   printf("-Z   | % 4.2f V % 5.0f mA \n",  voltage[map[MINUS_Z]], current[map[MINUS_Z]]);
   printf("Bat  | % 4.2f V % 5.0f mA \n", voltage[map[BAT]], current[map[BAT]]);
-  printf("Bat2 | % 4.2f V % 5.0f mA \n\n", voltage[map[BUS]], current[map[BUS]]);
+  printf("Bat2 | % 4.2f V % 5.0f mA \n\n", voltage[map[BAT2]], current[map[BAT2]]);
   fclose(file1);
 
   return 0;
