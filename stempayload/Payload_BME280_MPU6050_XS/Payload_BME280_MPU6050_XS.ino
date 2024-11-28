c2573c8484584c473838c64cad26f1725a751a6f
Alan Johnston <alan.b.johnston@gmail.com>
2024-11-27 21:52:47 -0500

    Update Payload_BME280_MPU6050_XS.ino changed to white/yellow

--- stempayload/Payload_BME280_MPU6050_XS/Payload_BME280_MPU6050_XS.ino
+++ stempayload/Payload_BME280_MPU6050_XS/Payload_BME280_MPU6050_XS.ino
@@ -34,8 +34,8 @@ MPU6050 mpu6050(Wire);
 long timer = 0;
 int bmePresent;
 int RXLED = 17;  // The RX LED has a defined Arduino pin
-int greenLED = 9;
-int blueLED = 8;
+int whiteLED = 9;
+int yellowLED = 8;
 int Sensor1 = 0;
 float Sensor2 = 0;
 float temp;
@@ -132,12 +132,12 @@ void setup() {
   delay(250);
   blink(500);
   delay(250);
-  led_set(greenLED, HIGH);
+  led_set(whiteLED, HIGH);
   delay(250);
-  led_set(greenLED, LOW);
-  led_set(blueLED, HIGH);
+  led_set(whiteLED, LOW);
+  led_set(yellowLED, HIGH);
   delay(250);
-  led_set(blueLED, LOW);
+  led_set(yellowLED, LOW);
 
   if (bme.begin(0x76)) {
     bmePresent = 1;
@@ -344,14 +344,14 @@ void loop() {
     }
  
     if (acceleration > 1.2 * rest)
-        led_set(greenLED, HIGH);
+        led_set(whiteLED, HIGH);
     else
-        led_set(greenLED, LOW);
+        led_set(whiteLED, LOW);
         
     if (rotation > 5)
-        led_set(blueLED, HIGH);
+        led_set(yellowLED, HIGH);
     else
-        led_set(blueLED, LOW);
+        led_set(yellowLED, LOW);
     }
 
     payload_loop(); // sensor extension read function defined in payload_extension.cpp	  
@@ -480,8 +480,8 @@ void blink_setup()
 #if defined __AVR_ATmega32U4__
   pinMode(RXLED, OUTPUT);  // Set RX LED as an output
   // TX LED is set as an output behind the scenes
-  pinMode(greenLED, OUTPUT);
-  pinMode(blueLED,OUTPUT);
+  pinMode(whiteLED, OUTPUT);
+  pinMode(yellowLED,OUTPUT);
 #endif
 
 #if defined(ARDUINO_ARCH_MBED_RP2040) && defined(ARDUINO_ARCH_RP2040)
@@ -554,9 +554,9 @@ delay(length);
 void led_set(int ledPin, bool state)
 {
 #if defined(ARDUINO_ARCH_STM32F0) || defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_STM32F3) || defined(ARDUINO_ARCH_STM32F4) || defined(ARDUINO_ARCH_STM32L4)
-  if (ledPin == greenLED)
+  if (ledPin == whiteLED)
     digitalWrite(PB9, state);
-  else if (ledPin == blueLED)
+  else if (ledPin == yellowLED)
     digitalWrite(PB8, state);    
 #endif
  
@@ -565,9 +565,9 @@ void led_set(int ledPin, bool state)
 #endif  
 
 #if defined (ARDUINO_ARCH_MBED_RP2040) || (ARDUINO_ARCH_RP2040)
-  if (ledPin == greenLED)
+  if (ledPin == whiteLED)
     digitalWrite(19, state);
-  else if (ledPin == blueLED)
+  else if (ledPin == yellowLED)
     digitalWrite(18, state);  
 #endif	
 }
