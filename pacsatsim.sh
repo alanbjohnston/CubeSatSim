d4d0fb542c87fb604bd290a9abe76cce6eeb07c7
Alan Johnston <alan.b.johnston@gmail.com>
2024-11-29 22:48:41 -0500

    Update pacsatsim.sh add -qd to dire wolf

--- pacsatsim.sh
+++ pacsatsim.sh
@@ -175,13 +175,13 @@ if [ "$choice" = "7" ] || [ "$choice" = "8" ]  || [ "$frequency" = "Serenity" ] 
 
   echo -e "Auto decoding 4800 bps AX.25 packets on $frequency Hz"
 
-  direwolf -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-4800.conf -t 0 &
+  direwolf -qd -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-4800.conf -t 0 &
 
 else
 
   echo -e "Auto decoding APRS Pacsat packets on $frequency Hz"
 
-  direwolf -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim.conf -t 0 &
+  direwolf -qd -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim.conf -t 0 &
 
 fi
 
