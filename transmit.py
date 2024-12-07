b5c9caf3d32bae7494ead99dcc5f47a1d151b587
Alan Johnston <alan.b.johnston@gmail.com>
2024-12-07 17:51:08 -0500

    Update transmit.py restart pacsatsim

--- transmit.py
+++ transmit.py
@@ -426,7 +426,7 @@ if __name__ == "__main__":
 			elif (mode == 'p'):
 				print("Pacsat")
 #				system('/home/pi/CubeSatSim/pacsatsim.sh')
-				system('sudo systemctl start pacsatsim')
+				system('sudo systemctl restart pacsatsim')
 				while True:
 					sleep(30)
 			elif (mode == 'p'):
