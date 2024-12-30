e574937c7aac87611d2529c53702889c3ad71db0
Alan Johnston <alan.b.johnston@gmail.com>
2024-12-30 13:36:56 -0500

    Update pacsat.sh remove frequency comments

--- groundstation/pacsat.sh
+++ groundstation/pacsat.sh
@@ -40,44 +40,27 @@ echo
 
 sudo systemctl restart pacsatsim
 
-value=`cat /home/pi/CubeSatSim/sim.cfg`
-echo "$value" > /dev/null
-set -- $value
+#value=`cat /home/pi/CubeSatSim/sim.cfg`
+#echo "$value" > /dev/null
+#set -- $value
 
-echo "Receive frequency is $8 MHz"	
-echo "Transmit frequency is $7 MHz"	
-echo 
-echo "To change, quit and type CubeSatSim/config -F"
-echo
+#echo "Receive frequency is $8 MHz"	
+#echo "Transmit frequency is $7 MHz"	
+#echo 
+#echo "To change, quit and type CubeSatSim/config -F"
+#echo
 
-frequency="$8e6"
+#frequency="$8e6"
 
 #echo "Note that the 'Tuned to' frequency will be different from the chosen frequency due to the way SDRs work."
 
 #echo
 
-if [[ $(arecord -l | grep "USB Audio Device") ]] ; then
 
-	echo -e "Direwolf using USB Sound Card (FM Transceiver) on $frequency Hz"
-	
-	direwolf -P+ -D1 -qd -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14.conf -t 0 &
+echo 
+echo "The Pacsat and Pacsat Ground Station are running on this Pi using audio loopback."
+echo
 
-else
-
-	echo -e "Direwolf using RTL-SDR on $frequency Hz"
-	
-	direwolf -P+ -D3 -r 48000 -qd -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat.conf -t 0 &
-	
-	sleep 5
-	
-	value=`aplay -l | grep "Loopback"`
-	echo "$value" > /dev/null
-	set -- $value
-	
-	#rtl_fm -M fm -f 144.39M -s 48k | aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1
-	rtl_fm -M fm -f $frequency -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1 &
-
-fi
 
 cd /home/pi/Desktop/PacSatGround_0.46m_linux/
 
