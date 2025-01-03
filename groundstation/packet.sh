84e5a7c2d695f6b279db42e3c7e779d202194eb6
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-03 14:18:58 -0500

    Update packet.sh added alsa stop/start

--- groundstation/packet.sh
+++ groundstation/packet.sh
@@ -26,6 +26,9 @@ sudo killall -9 CubicSDR &>/dev/null
 
 sudo killall -9 zenity &>/dev/null
 
+sudo /etc/init.d/alsa-utils stop
+sudo /etc/init.d/alsa-utils start
+
 echo
 
 frequency=$(zenity --list 2>/dev/null --width=410 --height=360 --title="Packet Decoding with Direwolf" --text="Choose the frequency for packet decoding" --column="kHz" --column="Application" 144390 "APRS US 2m" 434900 "CubeSatSim" 144800 "APRS European 2m" 145175 "APRS Australian 2m" Other "Choose another frequency" 145825 "APRS on ISS" 437100 "Serenity CubeSat 4800 bps" Serenity "Test Serenity CubeSat decoding with WAV file" APRS "Test APRS decoding with CubeSatSim WAV file")
