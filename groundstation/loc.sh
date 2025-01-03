0185e522fb0ca2ce634b4b33ea7c829a707b4662
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-03 04:19:01 -0500

    Update loc.sh add venv

--- groundstation/loc.sh
+++ groundstation/loc.sh
@@ -5,7 +5,7 @@ sudo killall -9 java &>/dev/null
 
 sudo killall -9 gpredict &>/dev/null
 
-python3 /home/pi/CubeSatSim/groundstation/loc-foxtelem.py
+/home/pi/venv/bin/python3 /home/pi/CubeSatSim/groundstation/loc-foxtelem.py
 
 #/usr/bin/gpredict
 
