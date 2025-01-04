f224a0ede08f5e878715632f498bd56e8ed33ee7
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-04 08:44:07 -0500

    Update rtl-tcp.sh add journalctl messages

--- groundstation/rtl-tcp.sh
+++ groundstation/rtl-tcp.sh
@@ -49,9 +49,10 @@ sudo killall -9 aplay &>/dev/null
 
 sudo killall -9 zenity &>/dev/null
 
-
 #sudo /bin/sh -c '/usr/local/bin/rtl_tcp -a $(hostname -I|cut -f1 -d " ")'
 sudo systemctl start rtl_tcp &>/dev/null
+
+sudo journalctl -af -u rtl_tcp
 
 sleep 5
 
