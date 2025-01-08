87d2ec4b9cb6f53110bcc3367551a192fd7b802e
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-08 08:47:45 -0500

    Update pacsat.sh remove &

--- groundstation/pacsat.sh
+++ groundstation/pacsat.sh
@@ -64,7 +64,7 @@ sleep 10
 
 #echo
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0  &
+direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0  # &
 #/usr/bin/x-terminal-emulator --geometry=120x40 -e "direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0"
 
 echo 
