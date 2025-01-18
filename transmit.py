846b231c66d5f3abf56bb544ce46c3a319e6bbef
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-18 14:03:55 -0500

    Update transmit.py changed p to P

--- transmit.py
+++ transmit.py
@@ -429,7 +429,7 @@ if __name__ == "__main__":
 				system('sudo systemctl restart pacsatsim')
 				while True:
 					sleep(30)
-			elif (mode == 'p'):
+			elif (mode == 'P'):
 				print("Pacsat Ground Station")
 				while True:
 					sleep(30)
