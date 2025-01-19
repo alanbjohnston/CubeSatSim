ef0608f5df50abca7b8bab39c966f7ebc6e4990d
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-19 10:55:53 -0500

    Update codecAO40.c moved variables

--- codecAO40.c
+++ codecAO40.c
@@ -1,12 +1,6 @@
 #include <string.h>
 #include "codecAO40.h"
 
-    unsigned char m_RS_block[RSBLOCKS][NROOTS]; /* RS parity blocks */
-    unsigned char m_encoded[SYMPBLOCK] ;       /* encoded symbols */
-    int m_encoded_bytes;               /* Byte counter for encode_data() */
-    int m_ileaver_index;               /* Byte counter for interleaver */
-    unsigned char m_conv_sr;           /* Convolutional encoder shift register state */
-
 /* ---------------------- */
 /* AO40 Encoder - Decoder */
 /* ---------------------- */
