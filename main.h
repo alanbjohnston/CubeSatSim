9e00834eb337f784a4f3a9ce3d8276c764c04dd0
Alan Johnston <alan.b.johnston@gmail.com>
2025-01-27 17:26:08 -0500

    Update main.h make buffer bigger

--- main.h
+++ main.h
@@ -118,7 +118,7 @@ int testCount = 0;
 long time_start;
 char cmdbuffer[1000];
 FILE * file1;
-short int buffer[2336400]; // max size for 10 frames count of BPSK
+short int buffer[5000000]; // 2336400]; // max size for 10 frames count of BPSK
 FILE *telem_file;	
 
 #define S_RATE	(48000) // (44100)
