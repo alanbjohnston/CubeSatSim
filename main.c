2f370a238dc6dee0a2c8074fc0f5a6a0c64572ee
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-01 09:00:05 -0500

    Update main.c SW valid, Ant 1 and 2

--- main.c
+++ main.c
@@ -2388,8 +2388,7 @@ void get_tlm_fc() {
 	source_bytes[extended + FC_EPS + 17] = source_bytes[extended + FC_EPS + 17] | 0x3f & (temp >> 2);
 	source_bytes[extended + FC_EPS + 18] = 0xff & (temp << 6);
 
-//	source_bytes[extended + FC_EPS + 45] = 0x0a;		
-	source_bytes[extended + 48] = 0x0a;
+	source_bytes[extended + 48] = 0x0c;  // Antenna 1 and 2 deployed
 	
 	source_bytes[extended + 49] = 0xff & ((unsigned long int)sequence >> 16);  // was 45 46
 	source_bytes[extended + 50] = 0xff & ((unsigned long int)sequence >> 8);
@@ -2405,19 +2404,13 @@ void get_tlm_fc() {
 		printf("Error opening command_count.txt!\n");
 	fclose(command_count_file);
 
-	source_bytes[extended + 52] = 0xfc & (groundCommandCount << 2);
+//	source_bytes[extended + 52] = 0xfc & (groundCommandCount << 2);  // doesn't work
 
-	source_bytes[extended + 53] = 0x40 & (SafeMode == 1);
+//	source_bytes[extended + 53] = 0x40 & (SafeMode == 1);  // doesn't work
 
-	/*
-	source_bytes[extended + 46] = 0x01;
-	source_bytes[extended + 47] = 0x02;
-	source_bytes[extended + 48] = 0x03;
-	source_bytes[extended + 49] = 0x04;
-	source_bytes[extended + 50] = 0x05;
-	source_bytes[extended + 51] = 0x06;
-	source_bytes[extended + 52] = 0x07;
-*/	
+	source_bytes[extended + 53] = 0x0f;  // SW valid 
+	source_bytes[extended + 54] = 0xf0;
+
 #endif
 	
 #ifdef FC_EM
