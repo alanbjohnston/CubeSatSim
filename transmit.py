ca10f464302089cba1370bbeca088071ad283677
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-03 09:13:53 -0500

    Update transmit.py turning transmit back on

--- transmit.py
+++ transmit.py
@@ -752,9 +752,9 @@ if __name__ == "__main__":
 			GPIO.setmode(GPIO.BCM)  # added to make Tx LED work on Pi 4
 			GPIO.setup(txLed, GPIO.OUT)
 			
-##			if (command_tx == True):
+			if (command_tx == True):
 #				system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f 434.9e6 -t float &")
-##				system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f " + tx + "e6 -t float &")
+				system("sudo nc -l 8080 | csdr convert_i16_f | csdr fir_interpolate_cc 2 | csdr dsb_fc | csdr bandpass_fir_fft_cc 0.002 0.06 0.01 | csdr fastagc_ff | sudo /home/pi/rpitx/sendiq -i /dev/stdin -s 96000 -f " + tx + "e6 -t float &")
 			print("Turning LED on/off and listening for carrier")
 			image_index = 1;
 			while 1:
