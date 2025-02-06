e17dc40aee28e4748b573657be55f8c295f801c7
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-06 13:03:15 -0500

    Create fc_block_decode.py

---
+++ groundstation/fc_block_decode.py
@@ -0,0 +1,23 @@
+import sys
+from os import system
+from time import sleep
+import logging
+logging.basicConfig(format='%(message)s')
+# logging.warning('CC-Warning!')
+
+if __name__ == "__main__":
+	debug_mode = False
+	counter = 1
+	if (len(sys.argv)) > 1:
+#        	print("There are arguments!")
+		if ('d' == sys.argv[1]):
+			debug_mode = True
+
+  print(debug_mode)
+	for line in sys.stdin:
+		if (debug_mode):
+			print(line, end =" ")
+			logging.warning(line)
+			
+		if ((line.find("data: ")) > 0):
+			print("\ndata block found!\n")
