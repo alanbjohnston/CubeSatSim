9de07e6566ba1eee40308b37b0e3cd5b891917cf
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-07 15:20:54 -0500

    Update fc_block_decode.py image_file as string

--- groundstation/fc_block_decode.py
+++ groundstation/fc_block_decode.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 #					try:
 					filename = "image_file" + str(image_count) + "." + str(image_index) + ".jpeg"
 #						system("/home/pi/ssdv/ssdv -d -J image_file " + filename)
-					process = subprocess.run(["/home/pi/ssdv/ssdv","-d","-J", image_file, filename], text=True)
+					process = subprocess.run(["/home/pi/ssdv/ssdv","-d","-J", "image_file", filename], text=True)
 					print("\n\n RESULT: \n")
 					print(process)
 					system("sudo cp " + filename + " /home/pi/CubeSatSim/groundstation/public_html/image_file.jpeg")
