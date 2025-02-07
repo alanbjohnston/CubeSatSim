a3241d23320861e8fbc452870506ac538a35f676
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-07 14:31:49 -0500

    Update fctelem.sh add python web server

--- groundstation/fctelem.sh
+++ groundstation/fctelem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# script to run OpenWebRX SDR
+# script to run FunCube Pi Telemetry App
 
-echo "Script to run Web SDR for ARISS Radio Pi"
+echo "Script to run FunCube CubeSatSim Telemetry"
 
 echo 
 
-echo "The Chromium browser will load in a few seconds with OpenWebRX."
+echo "The Chromium browser will load in a few seconds with fctelem."
 
 echo "You can also use another web browser if you are on the same network as your Pi."
 
@@ -14,7 +14,7 @@ echo
 
 ip=$(hostname -I|cut -f1 -d ' ')
 
-echo "IP Address to use in web browser is: $ip:8073"
+echo "IP Address to use in web browser is: $ip:8000"
 
 echo
 
@@ -44,11 +44,18 @@ sudo killall -9 zenity &>/dev/null
 
 sudo systemctl stop rtl_tcp
 
-sudo systemctl restart openwebrx
+sudo systemctl stop openwebrx
+
+
+cd /home/pi/CubeSatSim/groundstation/public_html
+
+cp /home/pi/CubeSatSim/groundstation/index.html .
+
+python3 -m http.server 
 
 sleep 10
 
-setsid chromium-browser --check-for-update-interval=1 --simulate-critical-update  --noerrdialogs --disable-infobars http://127.0.0.1:8073 &>/dev/null & 
+setsid chromium-browser --check-for-update-interval=1 --simulate-critical-update  --noerrdialogs --disable-infobars http://127.0.0.1:8000 &>/dev/null & 
 
 sleep 10
 
