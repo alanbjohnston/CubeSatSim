eb5d4e2f45a24bcc5f51430aa510155e49c60960
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-07 17:26:12 -0500

    Update fc_block_decode.py added &>

--- groundstation/fc_block_decode.py
+++ groundstation/fc_block_decode.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 						print("File error")
 #					try:
 					filename = "image_file" + str(image_count) + "." + str(image_index) + ".jpeg"
-					system("/home/pi/ssdv/ssdv -d -J image_file " + filename + " > ssdv_output")
+					system("/home/pi/ssdv/ssdv -d -J image_file " + filename + " &> ssdv_output")
 #					process = subprocess.run(["/home/pi/ssdv/ssdv","-d","-J", "image_file", filename], text=True)
 #					print("\n\n RESULT: \n")
 #					print(process)
