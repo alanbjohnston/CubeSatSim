b7710a08781b6e74addaf3220e8d10f5a2d887aa
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-08 23:00:35 -0500

    Update fc_block_decode.py formatting

--- groundstation/fc_block_decode.py
+++ groundstation/fc_block_decode.py
@@ -34,7 +34,7 @@ head_string = '<HEAD><meta http-equiv="refresh" content="5"></HEAD>\n<HTML>\n<H2
 foot_string = "</pre></p>\n</HTML>"
 telem_string = f"\nSequence number: {sequence:5d} Image ID: {image_id:3d} count: {image_count:2d}<p>" + \
 		f" Vx(mV): {Vx:5d}   Vy(mV): {Vy:5d}   Vz(mV): {Vz:5d}<p>" + \
-		f" Ix(mA): {Ix:5d}   Iy(mA): {Iy:5d}   Iz(mA): {Iz:5d}<p>  Vbat(mV): {Vb:5d}   Ibat(mA): {Ib:5d}"
+		f" Ix(mA): {Ix:5d}   Iy(mA): {Iy:5d}   Iz(mA): {Iz:5d}<p>    Vbat(mV): {Vb:5d}   Ibat(mA): {Ib:5d}"
 with open("/home/pi/CubeSatSim/groundstation/public_html/index.html", "w") as html_file:
 	html_file.write(head_string)
 	html_file.write(telem_string)
@@ -134,7 +134,7 @@ if __name__ == "__main__":
 								system("cp " + filename + " /home/pi/CubeSatSim/groundstation/public_html/image_file.jpeg")
 								telem_string = f"\nSequence number: {sequence:5d} Image ID: {image_id:3d} count: {image_count:2d}<p>" + \
 									f" Vx(mV): {Vx:5d}   Vy(mV): {Vy:5d}   Vz(mV): {Vz:5d}<p>" + \
-									f" Ix(mA): {Ix:5d}   Iy(mA): {Iy:5d}   Iz(mA): {Iz:5d}<p>  Vbat(mV): {Vb:5d}   Ibat(mA): {Ib:5d}"
+									f" Ix(mA): {Ix:5d}   Iy(mA): {Iy:5d}   Iz(mA): {Iz:5d}<p>    Vbat(mV): {Vb:5d}   Ibat(mA): {Ib:5d}"
 								with open("/home/pi/CubeSatSim/groundstation/public_html/index.html", "w") as html_file:
 									html_file.write(head_string)
 									html_file.write(telem_string)
