b95a31751f0b8d28466ab47bf43e55d8b5efad8d
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-08 22:14:28 -0500

    Update fc_block_decode.py add more \

--- groundstation/fc_block_decode.py
+++ groundstation/fc_block_decode.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
 					except:
 						print("File error")
 #					try:
-					system("/home/pi/ssdv/ssdv -d -J /home/pi/fctelem/image_file_payload " +
+					system("/home/pi/ssdv/ssdv -d -J /home/pi/fctelem/image_file_payload " + 
 					       "/home/pi/fctelem/image_file_payload.jpeg 2>&1 | sudo tee /home/pi/fctelem/ssdv_output")
 					with open("/home/pi/fctelem/ssdv_output", "r") as file:
 						for line in file:
@@ -126,8 +126,8 @@ if __name__ == "__main__":
 								filename = "/home/pi/fctelem/image_file" + str(image_id) + "." + str(image_count) + ".jpeg"	
 								system("/home/pi/ssdv/ssdv -d -J /home/pi/fctelem/image_file " + filename)	
 								system("cp " + filename + " /home/pi/CubeSatSim/groundstation/public_html/image_file.jpeg")
-								telem_string = f"\nSequence number: {sequence:5d} Image ID: {image_id:3d} count: {image_count:2d}<p>" +
-									"Vx: {Vx:5d} mV  Vy: {Vy:5d} mV  Vz: {Vz:5d} mV<p>Ix: {Ix:5d} mA  Iy: {Iy:5d} mA  Iz: {Iz:5d} mA<p>" +
+								telem_string = f"\nSequence number: {sequence:5d} Image ID: {image_id:3d} count: {image_count:2d}<p>" + \
+									"Vx: {Vx:5d} mV  Vy: {Vy:5d} mV  Vz: {Vz:5d} mV<p>Ix: {Ix:5d} mA  Iy: {Iy:5d} mA  Iz: {Iz:5d} mA<p>" + \
 									"Vb: {Vb:5d} mV  Ib: {Ib:5d} mA"
 								with open("/home/pi/CubeSatSim/groundstation/public_html/index.html", "w") as html_file:
 									html_file.write(head_string)
