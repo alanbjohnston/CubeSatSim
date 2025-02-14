5c584f62caab9c919e573317cdbeafdfe6d7aebe
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-14 16:41:23 -0500

    Update fc_block_decode.py add tlm_string

--- groundstation/fc_block_decode.py
+++ groundstation/fc_block_decode.py
@@ -167,6 +167,7 @@ if __name__ == "__main__":
 										for line in csv_file:
 											html_file.write(line)
 											html_file.write("<br>")
+									tlm_string = fstr(csv_format)		
 									html_file.write(tlm_string)
 									html_file.write("<br>")
 									html_file.write(foot_string)
