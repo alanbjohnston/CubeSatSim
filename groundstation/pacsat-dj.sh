4ae4f08851a65768696c923ab63ec412c2980f2c
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-25 16:19:30 -0500

    Update pacsat-dj.sh add fm

--- groundstation/pacsat-dj.sh
+++ groundstation/pacsat-dj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/x-terminal-emulator --geometry=120x40 -e "direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-jp14.conf -t 0"
+/usr/bin/x-terminal-emulator --geometry=120x40 -e "direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-fm-jp14.conf -t 0"
 
 
 
