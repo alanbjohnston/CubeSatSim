62f661d6b0fcfedfbd1fbd5e5c570208299c793e
Alan Johnston <alan.b.johnston@gmail.com>
2025-02-27 16:57:12 -0500

    Update transmit.py add sleeps

--- transmit.py
+++ transmit.py
@@ -369,7 +369,9 @@ if __name__ == "__main__":
 		for i in range(6):
 #			ser.write(b"AT+DMOSETGROUP=0,435.0000,434.9000,0,3,0,0\r\n")
 			ser.write(uhf_string1.encode())
+			sleep(0.1)
 			ser.write(connect_string.encode())
+			sleep(0.1)
 			ser.write(uhf_string2.encode())
 			sleep(0.1)
 		ser.close()
