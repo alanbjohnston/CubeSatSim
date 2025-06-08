090081ccc51efe8ad24fddac9e2ec18a9230ada6
Alan Johnston <alan.b.johnston@gmail.com>
2025-06-08 09:14:57 -0400

    Update sstv_decode_prompt.sh merge FIABv4

--- groundstation/sstv_decode_prompt.sh
+++ groundstation/sstv_decode_prompt.sh
@@ -26,6 +26,8 @@ sudo killall -9 java &>/dev/null
 
 sudo killall -9 CubicSDR &>/dev/null
 
+sudo killall -9 sdrpp &>/dev/null
+
 sudo killall -9 zenity &>/dev/null
 
 #echo "s" >> .mode
@@ -128,7 +130,7 @@ set -- $value
 
 #rtl_fm -M fm -f 434.9M -s 48k | aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1 
 #rtl_fm -M fm -f 434.9M -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
-rtl_fm -M fm -f $frequency -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -D hw:0,0 -r 48000 -t raw -f S16_LE -c 1
+rtl_fm -M fm -f $frequency -s 48k | tee >(aplay -D hw:${2:0:1},0,0 -r 48000 -t raw -f S16_LE -c 1) | aplay -r 48000 -t raw -f S16_LE -c 1
 
 sleep 5
 
