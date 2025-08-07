1d60c1723af2738c73947b5ce8b2db1f5c9dd716
Alan Johnston <alan.b.johnston@gmail.com>
2025-08-07 13:10:32 -0400

    Update main.c fix typo

--- main.c
+++ main.c
@@ -1283,16 +1283,16 @@ void get_tlm_fox() {
   if (failureMode == FAIL_SOLAR) {
 	  voltage[PLUS_X] = 0.0;
 	  current[PLUS_X] = 0.0;
-	  printf("+X Solar Panel Simulated Failure\n");
+	  printf("+X Solar Simulated Failure\n");
   }
   if (failureMode == FAIL_DEGRADE) {
 	  voltage[MINUS_X] = voltage[MINUS_X] * 0.5;
 	  current[MINUS_X] = current[MINUS_X] * 0.5;
-	  printf("-X Solar Panel Degredation Simulated Failure\n");	  
+	  printf("-X Solar Deg Simulated Failure\n");	  
   }
   if (failureMode == FAIL_SHORT) {
 	  voltage[MINUS_Y] = 0.0;
-	  printf("-Y Solar Panel Short Circuit Simulated Failure\n");	  
+	  printf("-Y Solar SC Simulated Failure\n");	  
   }
   if (mode == FSK)
     id = 7;
@@ -1625,9 +1625,11 @@ void get_tlm_fox() {
     
 //    printf("Command count: %d\n", groundCommandCount);
 	int simulated; 
-	simulted = sim_mode;  
-	if (failureMode != OFF)  
+	simulated = sim_mode;  
+	if (failureMode != OFF)  {
 		simulated == TRUE;
+		printf("Showing Simulted in FoxTelem\n");
+	}
 //  int status = STEMBoardFailure + SafeMode * 2 + sim_mode * 4 + PayloadFailure1 * 8 +    
     int status = STEMBoardFailure + SafeMode * 2 + simulated * 4 + PayloadFailure1 * 8 +
       (i2c_bus0 == OFF) * 16 + (i2c_bus1 == OFF) * 32 + (i2c_bus3 == OFF) * 64 + (camera == OFF) * 128 + groundCommandCount * 256;
