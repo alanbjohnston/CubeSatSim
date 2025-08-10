9522edd7b12cd89d1b17ace8b56535fe9b59acdd
Alan Johnston <alan.b.johnston@gmail.com>
2025-08-10 15:45:18 -0400

    Update transmit.py print fail mode

--- transmit.py
+++ transmit.py
@@ -19,6 +19,8 @@ def sim_failure_check():
 		cam_fail = False
 		file = open("/home/pi/CubeSatSim/failure_mode.txt")
 		fail_mode = file.read(2)
+		print("Fail_mode: ")
+		print(fail_mode)
 		if (fail_mode == "10"):
 			card = "Device"  # Change audio so no FM audio plays
 			print("Failure mode no FM audio")
