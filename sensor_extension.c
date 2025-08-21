2a9161bc9e5e28f54f467c426a21f0a85ff0f76c
Alan Johnston <alan.b.johnston@gmail.com>
2025-08-21 13:35:26 -0400

    Create sensor_extension.c copied from payload_extension.cpp

---
+++ sensor_extension.c
@@ -0,0 +1,25 @@
+// Use this template for adding additional sensors
+// see Payload_BME280_MPU6050_XS_Extended for an example
+
+// put your library includes here
+#include "Arduino.h"
+
+// put your globals here
+
+// put your setup code here
+void payload_setup() {
+  
+//  Serial.println("Starting new sensor!");
+
+}
+
+// put your loop code here
+// Very Important: only use print, not println!!
+void payload_loop() {
+
+/*  
+  Serial1.print(" NEW 0.0");  // send sensor data over serial to Pi Zero
+  Serial.print(" NEW 0.0");   // send sensor data over serial monitor for testing
+*/
+  
+}
