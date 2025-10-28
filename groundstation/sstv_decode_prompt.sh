910d02b597038604ea822a0e7cf561485c767200
Alan Johnston <alan.b.johnston@gmail.com>
2025-10-27 22:56:42 -0400

    Update sstv_decode_prompt.sh add venv

--- groundstation/sstv_decode_prompt.sh
+++ groundstation/sstv_decode_prompt.sh
@@ -121,6 +121,7 @@ setsid qsstv &
 
 sleep 5
 
+source /home/pi/venv/bin/activate
 python3 /home/pi/CubeSatSim/groundstation/auto-tune.py 434900000 n 2> null > /home/pi/CubeSatSim/groundstation/auto-tune.txt
 echo "auto-tune.txt"
 cat /home/pi/CubeSatSim/groundstation/auto-tune.txt
