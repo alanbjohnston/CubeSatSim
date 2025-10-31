227c88a893cd7635070f06e6dc9a678d8142c508
Alan Johnston <alan.b.johnston@gmail.com>
2025-10-31 11:10:36 -0400

    Update pacsatsim-djf.sh change to half duplex

--- pacsatsim-djf.sh
+++ pacsatsim-djf.sh
@@ -2,4 +2,4 @@
 
 sudo modprobe snd-aloop
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14.conf -t 0 
+direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14-half.conf -t 0 
