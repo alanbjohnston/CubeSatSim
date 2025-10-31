c52a6f3e0ca968513aabd1efcca3de4ba0fdeb44
Alan Johnston <alan.b.johnston@gmail.com>
2025-10-31 16:44:03 -0400

    Update pacsatsim-d.sh fix filename

--- pacsatsim-d.sh
+++ pacsatsim-d.sh
@@ -2,4 +2,4 @@
 
 sudo modprobe snd-aloop
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-j14-loop.conf -t 0 
+direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-jp14-loop.conf -t 0 
