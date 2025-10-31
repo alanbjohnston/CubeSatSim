1265d27020cc437a4f7669cd4a6c16174ddd0eb7
Alan Johnston <alan.b.johnston@gmail.com>
2025-10-31 16:59:17 -0400

    Update pacsat-d.sh update with j14-loop

--- groundstation/pacsat-d.sh
+++ groundstation/pacsat-d.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/x-terminal-emulator --geometry=120x40 -e "direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0"
+sudo modprobe snd-aloop
 
-
+direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14-loop.conf -t 0 
 
