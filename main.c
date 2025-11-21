6e5f2055cec50f345680c56ec050d70f92f77709
Alan Johnston <alan.b.johnston@gmail.com>
2025-11-20 22:04:38 -0500

    Update main.c remove macro

--- main.c
+++ main.c
@@ -19,7 +19,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define PI_SENSORS
 #include "main.h"
 
 //#define HAB  // uncomment to change APRS icon from Satellite to Balloon and only BAT telemetry
