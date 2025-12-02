bf572cc9bccba0cee2e5d46ccb8c249506b95349
Alan Johnston <alan.b.johnston@gmail.com>
2025-12-01 21:55:25 -0500

    Update pacsat-df.sh add pwm variable

--- groundstation/pacsat-df.sh
+++ groundstation/pacsat-df.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+pwm=1
+
 sudo modprobe snd-aloop
 
 #gpio -g mode 20 out
 
 #gpio -g write 20 1
 
-direwolf -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14.conf -t 0 
+#direwolf -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14.conf -t 0 
 
+if [ "$pwm" = "1" ] ; then  
+
+  direwolf -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-pwm.conf -t 0 
+ 
+else
+
+  direwolf -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14.conf -t 0 
+  
+fi
