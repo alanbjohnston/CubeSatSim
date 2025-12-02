67fde490cfbfa41682789db282d506bf8dc7d33d
Alan Johnston <alan.b.johnston@gmail.com>
2025-12-01 21:53:59 -0500

    Update pacsatsim-df.sh add pwm variable

--- pacsatsim-df.sh
+++ pacsatsim-df.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+pwm=1
+
 sudo modprobe snd-aloop
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-jp14.conf -t 0 
+if [ "$pwm" = "1" ] ; then  
+
+  direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-pwm.conf -t 0 
+
+else
+
+  direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-jp14.conf -t 0 
+
+fi
