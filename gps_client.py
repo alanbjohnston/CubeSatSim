e4fa0f7082fbf1e090356b34cc65809f836e605f
Alan Johnston <alan.b.johnston@gmail.com>
2025-12-14 07:25:05 -0500

    Update gps_client.py

--- gps_client.py
+++ gps_client.py
@@ -16,7 +16,7 @@ session = gps.gps(mode=gps.WATCH_ENABLE)
 start = time.perf_counter()
 
 try:
-    while session.read() == 0 and ((time.perf_counter(0 - start)) < 1):
+    while session.read() == 0 and ((time.perf_counter() - start) < 1):
         print(gps.MODE_SET)
         print(session.valid)
         if not (gps.MODE_SET & session.valid):
