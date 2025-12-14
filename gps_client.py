dd0b73c2cbe0742e3539360daa7300b074fef821
Alan Johnston <alan.b.johnston@gmail.com>
2025-12-14 07:36:34 -0500

    Update gps_client.py fix prints

--- gps_client.py
+++ gps_client.py
@@ -46,7 +46,7 @@ try:
         if ((gps.isfinite(session.fix.latitude) and
              gps.isfinite(session.fix.longitude))):
             print("%.6f %.6f %.6f" %
-                  (session.fix.latitude, session.fix.longitude), session.fix.altitude)
+                  (session.fix.latitude, session.fix.longitude, session.fix.altitude))
             lat = session.fix.latitude
             lon = session.fix.longitude 
             alt = session.fix.altitude     
@@ -59,5 +59,5 @@ except KeyboardInterrupt:
 
 # Got ^C, or fell out of the loop.  Cleanup, and leave.
 session.close()
-print("%.6f %.6f %.1f" % lat, lon, alt)
+print("%.6f %.6f %.1f" % (lat, lon, alt))
 exit(0)
