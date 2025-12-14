ac265777711a45d4ae66e0367af3af0f591a2827
Alan Johnston <alan.b.johnston@gmail.com>
2025-12-14 07:34:56 -0500

    Update gps_client.py fix print

--- gps_client.py
+++ gps_client.py
@@ -59,5 +59,5 @@ except KeyboardInterrupt:
 
 # Got ^C, or fell out of the loop.  Cleanup, and leave.
 session.close()
-print("%.6f %.6f %.1" % lat, lon, alt)
+print("%.6f %.6f %.1f" % lat, lon, alt)
 exit(0)
