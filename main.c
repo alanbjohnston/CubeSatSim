e8e5afaf481ff7d9feac81463e29c7e0255f81b8
Alan Johnston <alan.b.johnston@gmail.com>
2026-01-26 12:56:57 -0500

    Update main.c remove error_count reset

--- main.c
+++ main.c
@@ -2983,7 +2983,7 @@ void socket_send(int length) {
     struct sockaddr_in serv_addr;
     //    char *hello = "Hello from client"; 
     //    char buffer[1024] = {0};
-	error_count = 0;
+	// error_count = 0;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
       printf("\n Socket creation error \n");
       error = 1;
