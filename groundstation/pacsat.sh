6532c062840569ccdb33481f42aa0856c9deafc9
Alan Johnston <alan.b.johnston@gmail.com>
2026-02-07 22:46:08 -0500

    Update pacsat.sh add pi user to gpio

--- groundstation/pacsat.sh
+++ groundstation/pacsat.sh
@@ -77,6 +77,8 @@ echo
 #direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0  # &
 #/usr/bin/x-terminal-emulator --geometry=120x40 -e "direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0"
 
+sudo usermod -a -G gpio pi
+
 /usr/bin/x-terminal-emulator --geometry=120x40 -e "/home/pi/CubeSatSim/groundstation/pacsat-df.sh"
 
 # arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 14000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 435045 &
