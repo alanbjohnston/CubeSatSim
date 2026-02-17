a63c5aa656ee4a60124a2586509cffb405299923
Alan Johnston <alan.b.johnston@gmail.com>
2026-02-17 10:13:49 -0500

    Update main.c change file desc name

--- main.c
+++ main.c
@@ -323,14 +323,14 @@ int main(int argc, char * argv[]) {
       snprintf(busStr, 10, "%d %d", i2c_bus1, i2c_bus3);
   }
 
-  FILE * os-test = popen("cat /etc/os-release", "r");
-  fgets(cmdbuffer, 1000, os-test);
-  printf("os-release: %s\n", cmdbuffer);
+  FILE * os_test = popen("cat /etc/os-release", "r");
+  fgets(cmdbuffer, 1000, os_test);
+  printf("os-release: %s\n" cmdbuffer);
   char os_present[] = "bookworm";
   // printf("strstr: %s \n", strstr( & cmdbuffer1, camera_present));
   int os_status = (strstr( (const char *)& cmdbuffer, os_present) != NULL) ? ON : OFF;	
   printf(os_status);
-  pclose (os-test);
+  pclose (os_test);
 	
   // check for camera	
 //  char cmdbuffer1[1000];
