0cab7f0623361d2a2f6bcc78ad60972c18cda5a7
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-02 14:56:48 -0500

    Update pacsat-d.sh change to loopback

--- groundstation/pacsat-d.sh
+++ groundstation/pacsat-d.sh
@@ -2,5 +2,4 @@
 
 sudo modprobe snd-aloop
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-fm-pacsat-jp14-loop.conf -t 0 
-
+direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/groundstation/direwolf/direwolf-pacsat-loopback.conf -t 0 
