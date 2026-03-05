42aa30f83d7a4363749fd0e5fa1721a3644d2b9e
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-05 13:49:54 -0500

    Update cubicsdr.sh resolve merge conflict

--- groundstation/cubicsdr.sh
+++ groundstation/cubicsdr.sh
@@ -31,10 +31,30 @@ sudo killall -9 rtl_tcp &>/dev/null
 
 sudo killall -9 CubicSDR &>/dev/null
 
+sudo killall -9 sdrpp &>/dev/null
+
 #sudo kill `ps -aux | grep cubicsdr-packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
 #sudo kill `ps -aux | grep packet | grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
+#sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
+
+sudo killall -9 zenity &>/dev/null
+
+sudo /etc/init.d/alsa-utils stop
+sudo /etc/init.d/alsa-utils start
+
+sudo killall -9 rtl_fm &>/dev/null
+
+zenity --info --width=650 --height=140 --title="Instructions" --text="When CubicSDR opens, select <i>Generic RTL2832U</i> device then click <b>Start</b> to begin.\n\nThen click on a signal in the watefall to listen." &
+
+sleep 5
+
+setsid CubicSDR
+
+sleep 10
+
+#$SHELL
 #sudo kill `ps -aux | grep sstv_decode_prompt| grep -v grep | awk '{ print $2 }'` &>/dev/null && killall inotifywait &>/dev/null
 
 sudo killall -9 zenity &>/dev/null
