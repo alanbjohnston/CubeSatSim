f3c4d4b3adb75bed70a9f8b09c3a3204ed255c20
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-06 05:04:50 -0500

    Update main.c remove extra }

--- main.c
+++ main.c
@@ -601,8 +601,6 @@ int main(int argc, char * argv[]) {
 	
   if ((mode == FSK) || (mode == BPSK) || (mode == PACSAT)) // && !sim_mode)
       get_tlm_fox();	// fill transmit buffer with reset count 0 packets that will be ignored
-//	  get_tlm_fox();
-  }
   else if (mode == FC) // && !sim_mode)
       get_tlm_fc();	// fill transmit buffer with reset count 0 packets that will be ignored
   else if (mode == CW)
