3ad7cdb6f9bc9e4127711a2b3ebf7ba566a93619
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-14 14:20:36 -0400

    Update pacsatsim.sh safe mode read frequency from sim.cfg

--- pacsatsim.sh
+++ pacsatsim.sh
@@ -58,6 +58,7 @@ echo "$value" > /dev/null
 set -- $value
 
 callsign="$1"
+frequency="$7e3"
 
 echo "Configured callsign is "
 echo $callsign
@@ -155,7 +156,8 @@ if [ "$safe" = "1" ] ; then
 
   direwolf -P+ -D1 -qd -dp -r 48000 -c /home/pi/CubeSatSim/direwolf/direwolf-pacsatsim-pwm-loopback.conf -t 0 &
 
-  arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434900 &
+#  arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434900 &
+  arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f $frequency &
 
 
 elif [ "$loopback" = "1" ] ; then
