d720f030552ef7a8ad9dcfab80fdadaa9c8a2f85
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-16 11:48:02 -0400

    Update transmit.py if beacon on, restart pacsatsim

--- transmit.py
+++ transmit.py
@@ -551,7 +551,8 @@ if __name__ == "__main__":
 					print("Pacsat Ground Station")
 				else:
 					print("Pacsat")
-#					system("sudo systemctl restart pacsatsim")
+					if (command_tx == True):
+						system("sudo systemctl restart pacsatsim")
 				output(txLed, 0)
 				print("0")
 				rpitx = "arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f " + tx + "e3 > /dev/null 2>&1 &"
