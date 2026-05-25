fe509794895b19b7acf8ebaf18336d76ea2723aa
Alan Johnston <alan.b.johnston@gmail.com>
2026-05-24 23:03:04 -0400

    Update pacsatsim.sh change pacsatsim files to v0.2

--- pacsatsim.sh
+++ pacsatsim.sh
@@ -84,11 +84,11 @@ if [ ! -d "/home/pi/PacSat" ]; then
 
   cd
   sudo rm pacsat-dir.zip
-  mkdir PacSat
-  mkdir PacSat/pacsat
-  mkdir PacSat/pacsat/dir
-  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.1/pacsat-dir.zip
-  unzip pacsat-dir.zip -d PacSat/pacsat/dir
+  mkdir /home/pi/PacSat
+  mkdir /home/pi/PacSat/pacsat
+  mkdir /home/pi/PacSat/pacsat/dir
+  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pacsat-dir.zip
+  unzip pacsat-dir.zip -d /home/pi/PacSat/pacsat/dir
   sudo rm pacsat-dir.zip
 
   cd
@@ -96,7 +96,7 @@ if [ ! -d "/home/pi/PacSat" ]; then
   sudo rm /home/pi/pi_pacsat/Debug/pacsat.state
   sudo rm /home/pi/pi_pacsat/Debug/pacsat_upload_table.dat
   sudo rm pi_pacsat.zip
-  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.1/pi_pacsat.zip
+  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pi_pacsat.zip
   unzip pi_pacsat.zip -d /home/pi/pi_pacsat/Debug 
   sudo rm pi_pacsat.zip
         
