e0001319e2f62aadcdcb48b78757489b0d42e537
Alan Johnston <alan.b.johnston@gmail.com>
2026-05-31 23:24:51 -0400

    Update pacsatsim.sh simple dire wolf output

--- pacsatsim.sh
+++ pacsatsim.sh
@@ -298,7 +298,8 @@ echo
 cat $DIREWOLF_CONF
 echo
 
-direwolf -P+ -D1 -qd -dp -r 48000 -c $DIREWOLF_CONF -t 0 &
+#direwolf -P+ -D1 -qd -dp -r 48000 -c $DIREWOLF_CONF -t 0 &
+direwolf -qd -r 48000 -c $DIREWOLF_CONF &
 
 #  arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 14000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434900 &
 ## arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx -i- -m RF -f 434900 &
