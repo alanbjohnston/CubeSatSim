bebab685b8cf33889cdf779039f1904f69ac9278
Alan Johnston <alan.b.johnston@gmail.com>
2026-05-31 23:18:02 -0400

    Update pacsatsim.sh copy don't wget zip files

--- pacsatsim.sh
+++ pacsatsim.sh
@@ -87,7 +87,8 @@ if [ ! -d "/home/pi/PacSat" ]; then
   mkdir /home/pi/PacSat
   mkdir /home/pi/PacSat/pacsat
   mkdir /home/pi/PacSat/pacsat/dir
-  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pacsat-dir.zip
+#  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pacsat-dir.zip
+  cp /home/pi/pi_pacsat/zip/pacsat-dir.zip .
   unzip pacsat-dir.zip 
   sudo rm pacsat-dir.zip
 
@@ -96,7 +97,8 @@ if [ ! -d "/home/pi/PacSat" ]; then
   sudo rm /home/pi/pi_pacsat/Debug/pacsat.state
   sudo rm /home/pi/pi_pacsat/Debug/pacsat_upload_table.dat
   sudo rm pi_pacsat.zip
-  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pi_pacsat.zip
+#  wget https://github.com/alanbjohnston/pi_pacsat/releases/download/v0.2/pi_pacsat.zip
+  cp /home/pi/pi_pacsat/zip/pi_pacsat.zip .
   unzip pi_pacsat.zip -d /
   sudo rm pi_pacsat.zip
         
