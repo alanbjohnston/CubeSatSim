26c9d36b76db64004b63b571bad7b5c1abea5a9c
Alan Johnston <alan.b.johnston@gmail.com>
2026-07-05 11:17:23 -0400

    Update transmit.py move &

--- transmit.py
+++ transmit.py
@@ -621,7 +621,7 @@ def transmit_carrier(duration):
 	global tx_doppler_freq_hz
 	global txLed
 #	command = "timeout " + str(duration) + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" # 434.9e6
-	start = "sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1 &" # 434.9e6
+	start = "sudo tune -f " + str(tx_doppler_freq_hz) + " & > /dev/null 2>&1" # 434.9e6
 	stop = "sudo killall -9 tune"
 	output(txLed, 1)
 	system(start)
