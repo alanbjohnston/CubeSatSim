7cfe468966a12aa8abb21770f943c1c147c47db5
Alan Johnston <alan.b.johnston@gmail.com>
2026-07-06 16:04:34 -0400

    Update transmit.py switch back to timeout

--- transmit.py
+++ transmit.py
@@ -620,18 +620,19 @@ def cw_transmit_char(character):
 def transmit_carrier(duration):
 	global tx_doppler_freq_hz
 	global txLed
-#	command = "timeout " + str(duration) + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" # 434.9e6
+	command = "timeout " + str(duration) + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" # 434.9e6
 	start = "sudo tune -f " + str(tx_doppler_freq_hz) + " &" # + " & > /dev/null 2>&1" # 434.9e6
 	stop = "sudo rpitx -f 434.9e6 &"
 	killrpitx = "sudo killall -9 rpitx &"
 	killtune = "sudo killall -9 tune &"
 	output(txLed, 1)
-	system(start)
-	sleep(duration)
-	system(stop)
+#	system(start)
+	system(command)
+#	sleep(duration)
+#	system(stop)
 	output(txLed, 0)
-	system(killrpitx)
-	system(killtune)
+#	system(killrpitx)
+#	system(killtune)
 
 print("CubeSatSim v2.2 transmit.py starting...")
 
