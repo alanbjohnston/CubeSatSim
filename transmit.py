446f9195f3d46e7f1590aadd9a5fccad39a49eb2
Alan Johnston <alan.b.johnston@gmail.com>
2026-07-13 11:33:41 -0400

    Update transmit.py change timeout to timelimit

--- transmit.py
+++ transmit.py
@@ -625,7 +625,9 @@ def transmit_carrier(duration):
 	global tx_doppler_freq_hz
 	global txLed
 	update_doppler("no")
-	command = "timeout " + str(duration) + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" # 434.9e6
+	dur_str = str(duration)
+#	command = "timeout " + str(duration) + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" # 434.9e6
+	command = "timelimit -t" + dur_str + " -T" + dur_str + " sudo tune -f " + str(tx_doppler_freq_hz) + " > /dev/null 2>&1" 
 	start = "sudo tune -f " + str(tx_doppler_freq_hz) + " &" # + " & > /dev/null 2>&1" # 434.9e6
 	stop = "sudo rpitx -f 434.9e6 &"
 	killrpitx = "sudo killall -9 rpitx &"
