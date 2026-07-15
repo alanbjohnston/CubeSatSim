89797c88c2c158bf2476a8b7b8d4cb58c01af085
Alan Johnston <alan.b.johnston@gmail.com>
2026-07-15 18:43:52 -0400

    Update transmit.py fix variable with dash

--- transmit.py
+++ transmit.py
@@ -1002,7 +1002,7 @@ if __name__ == "__main__":
 						print("Starting PacSatSim")
 				output(txLed, 0)
 				print("0")
-				rpitx-ui = "arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx-ui -i- -m RF -f " + tx + "e3 > /dev/null 2>&1 &"
+				rpitx = "arecord -D plughw:CARD=Loopback,DEV=1 -f S16_LE -r 48000 -c 1 | csdr convert_s16_f | csdr gain_ff 4000 | csdr convert_f_samplerf 20833 | sudo rpitx-ui -i- -m RF -f " + tx + "e3 > /dev/null 2>&1 &"
 				stop_rpitx = "sudo killall -9 rpitx-ui && sudo killall -9 arecord && sudo rpitx-ui -m RF -f 434.9e3 > /dev/null 2>&1"
 				if not txc:
 					print("txc is:")
