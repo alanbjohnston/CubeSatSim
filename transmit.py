b5fc1b78900eaebbd8f3448b9f7f6be7c39f5cb7
Alan Johnston <alan.b.johnston@gmail.com>
2026-08-10 16:59:43 -0400

    Update transmit.py add command_tx for CW doppler

--- transmit.py
+++ transmit.py
@@ -1048,17 +1048,18 @@ if __name__ == "__main__":
 ##					ch = 1
 					for chan in range(7):
 						if (doppler_mode):
-							try:
-#								update_doppler()
-								filename="/home/pi/CubeSatSim/cw" + str(chan) + ".txt"
-								print(filename)
-								file = open(filename)
-								cw_string = file.readline()
-								print(cw_string)
-								cw_transmit_string(cw_string)
-							except Exception as e:
-								print(f"An error occurred: {e}")
-								print("error reading cw string " + str(chan))
+							if (command_tx == True):
+								try:
+	#								update_doppler()
+									filename="/home/pi/CubeSatSim/cw" + str(chan) + ".txt"
+									print(filename)
+									file = open(filename)
+									cw_string = file.readline()
+									print(cw_string)
+									cw_transmit_string(cw_string)
+								except Exception as e:
+									print(f"An error occurred: {e}")
+									print("error reading cw string " + str(chan))
 						else:	
 							command = "gen_packets -M 20 -o /home/pi/CubeSatSim/morse.wav /home/pi/CubeSatSim/cw" + str(chan) + ".txt -r 48000 > /dev/null 2>&1"
 							print(command)
